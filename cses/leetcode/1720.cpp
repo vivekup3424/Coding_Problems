0b0628d221dc1dde87402789600c6430602d88fa
vivekup3424 <vivek.hygfrt324@gmail.com>
2023-08-17 22:39:01 +0530

    17th August 2023 commit

---
+++ cses/leetcode/1720.cpp
@@ -0,0 +1,18 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        encoded.insert(encoded.begin(), first);
+        for(int i = 1;i<encoded.size();i++)
+        {
+            encoded[i] = encoded[i]^encoded[i-1];
+        }        
+        return encoded;
+    }
+};
+int main()
+{
+    
+}
