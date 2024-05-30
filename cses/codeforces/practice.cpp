fc25d64374b5dbc93cea43ced2de0b33cf4e6058
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-05-30 22:11:18 +0530

    new contest

--- cses/codeforces/practice.cpp
+++ cses/codeforces/practice.cpp
@@ -1,25 +1,51 @@
-#include <algorithm>
-#include <cstdio>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+typedef long long unsigned llu;
+typedef std::vector<int> vi;
+typedef std::vector<long long> vll;
+typedef std::vector<vector<long long>> vvll;
+const int MOD = 1e9 + 7;
 
+inline void fast_io()
+{
+    ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    cout.tie(NULL);
+}
 
-int main(){
-    frepoen("shall.in","r",stdin);
-    int n;
-    scanf("%d",&n);
-    vector<int> shell_at_pos(3);
-    for (int i = 0; i < 3; i++)
+// Template function to input elements to a vector
+template <typename T>
+void inputVector(vector<T> &v, int n)
+{
+    v.resize(n);
+    for (int i = 0; i < n; i++)
     {
-        shell_at_posp[i] = i;
+        std::cin >> v[i];
     }
-    vector<int>counter(3);
-    //number of times the shell with number i was picked
+}
 
-    for(int i = 0; i < n; i++){
-        int a,b,g;
-        scanf("%d %d %d", &a,&b,&g);
-        
+// Template function to output elements of a vector
+template <typename T>
+void outputVector(const vector<T> &v, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
     }
-    
+    cout << "\n";
+}
+
+int main()
+{
+    fast_io();
+    ll T;
+    std::cin >> T;
+    while (T--)
+    {
+        ll n, m;
+        cin >> n >> m;
+        ll total = n + m + 1;
+        vll prog_skills
+    }
 }
