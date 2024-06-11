b192971eacc8f1548e5e7217de50e2d2b0f9ae9e
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-06-11 14:52:01 +0530

    aff

--- cses/codeforces/1984C.cpp
+++ cses/codeforces/1984C.cpp
@@ -1,64 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef long long unsigned llu;
-typedef std::vector<int> vi;
-typedef std::vector<long long> vll;
-typedef std::vector<vector<long long>> vvll;
-const int MOD = 1e9 + 7;
-
-inline void fast_io()
-{
-    ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    cout.tie(NULL);
-}
-
-// Template function to input elements to a vector
-template <typename T>
-void inputVector(vector<T> &v, int n)
-{
-    v.resize(n);
-    for (int i = 0; i < n; i++)
-    {
-        std::cin >> v[i];
-    }
-}
-
-// Template function to output elements of a vector
-template <typename T>
-void outputVector(const vector<T> &v, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cout << v[i] << " ";
-    }
-    cout << "\n";
-}
-ll getSum(vll v, ll n, ll idx, ll sum)
-{
-    if (idx == n)
-    {
-        return sum;
-    }
-    else
-    {
-        return max(
-            getSum(v, n, idx + 1, sum + v[idx]),
-            getSum(v, n, idx + 1, abs(sum + v[idx])));
-    }
-}
 int main()
 {
-    fast_io();
-    ll T;
-    std::cin >> T;
-    while (T--)
+    int t;
+    cin >> t;
+    for (; t > 0; t--)
     {
-        ll n;
+        int n;
         cin >> n;
-        vll v(n);
-        inputVector(v, n);
-        cout << getSum(v, n, 0, 0) << endl;
+        vector<int> a(n);
+        int sum = 0, mini = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            sum += a[i];
+            mini = min(mini, sum);
+        }
+        cout << sum - 2 * mini << endl;
     }
 }
