3552910ecb02879090f1365bfb66de8deeafa1bf
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-06-20 11:51:13 +0530

    koko eating banana

--- cses/leetcode/cpp/875.cpp
+++ cses/leetcode/cpp/875.cpp
@@ -3,11 +3,38 @@ using namespace std;
 class Solution
 {
 public:
+    int calculateTime(vector<int> &piles, int speed)
+    {
+        int total = 0;
+        for (int i = 0; i < piles.size(); i++)
+        {
+            total += piles[i] / speed;
+            if (piles[i] % speed > 0)
+            {
+                total++;
+            }
+        }
+        return total;
+    }
     int minEatingSpeed(vector<int> &piles, int h)
     {
+        int l = 1, r = *max_element(piles.begin(), piles.end());
+        sort(piles.begin(), piles.end());
+        int ans = r;
+        while (l <= r)
+        {
+            int mid = (l + r) / 2;
+            int time = calculateTime(piles, mid);
+            if (time <= h)
+            {
+                ans = mid;
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+        return ans;
     }
 };
-int main()
-{
-    
-}
