8cf3c9a4c4b211ade8c331079ea3de7950add07d
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-06-20 14:42:52 +0530

    121. Best Time to Buy and Sell Stock

---
+++ cses/leetcode/go/121.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func maxProfit(prices []int) int {
+	maxSum, currentSum := 0, 0
+	for i := 0; i < len(prices)-1; i++ {
+		currentSum += prices[i+1] - prices[i]
+		if currentSum < 0 {
+			currentSum = 0
+		}
+		maxSum = max(maxSum, currentSum)
+	}
+	return maxSum
+}
+func main() {
+	writer := bufio.NewWriter(os.Stdout)
+	writer.Write([]byte("Enter the length of the array: "))
+	writer.Flush()
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	n, _ := strconv.Atoi(input)
+	numbers := make([]int, n)
+	inputValues, _ := reader.ReadString('\n')
+	values := strings.Split(inputValues, " ")
+	for i := 0; i < n; i++ {
+		v, _ := strconv.Atoi(values[i])
+		numbers[i] = v
+	}
+	fmt.Println(maxProfit(numbers))
+}
