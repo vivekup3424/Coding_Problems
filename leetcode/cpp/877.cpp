9a5d80258454a39a8152f82101cb210de0cea5b4
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-06-25 19:49:02 +0530

    min_max algo for stone game

--- leetcode/cpp/877.cpp
+++ leetcode/cpp/877.cpp
@@ -34,6 +34,49 @@ public:
         return sum1 > sum2;
     }
 };
+int recurse(vector<int> &piles, int i, int j, int turn)
+{
+    if (i > j)
+    {
+        return 0;
+    }
+    else if (i == j)
+    {
+        return piles[i];
+    }
+    if (turn == 0)
+    {
+        return max(piles[i] + recurse(piles, i + 1, j, !turn),
+                   piles[j] + recurse(piles, i, j - 1, !turn));
+    }
+    else if (turn == 1)
+    {
+        return min(piles[i] + recurse(piles, i + 1, j, !turn),
+                   piles[j] + recurse(piles, i, j - 1, !turn));
+    }
+}
+int recurse(vector<int> &piles, int i, int j, int turn)
+{
+    if (i > j)
+    {
+        return 0;
+    }
+    else if (i == j)
+    {
+        return piles[i];
+    }
+    if (turn == 0)
+    {
+        return max(piles[i] + recurse(piles, i + 1, j, !turn),
+                   piles[j] + recurse(piles, i, j - 1, !turn));
+    }
+    else if (turn == 1)
+    {
+        return min(piles[i] + recurse(piles, i + 1, j, !turn),
+                   piles[j] + recurse(piles, i, j - 1, !turn));
+    }
+}
+
 int main()
 {
     vector<int> piles = {3, 2, 10, 4};
