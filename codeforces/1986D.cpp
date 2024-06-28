174edec8601cb7b6426c0ce221da41bf332cc3e7
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-06-28 20:00:16 +0530

    mini push

--- codeforces/1986D.cpp
+++ codeforces/1986D.cpp
@@ -1,135 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 typedef long long ll;
-
-string trimZero(const string &s)
+int main(int argc, char const *argv[])
 {
-    int i = 0;
-    // Skip leading zeros
-    while (i < s.length() && s[i] == '0')
-    {
-        i++;
-    }
-    // Append remaining characters
-    string temp;
-    while (i < s.length())
-    {
-        temp.push_back(s[i]);
-        i++; // Increment i to move to the next character
-    }
-    return temp;
-}
-
-bool containsZero(const string &s)
-{
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (s[i] == '0')
-        {
-            return true;
-        }
-    }
-    return false;
-}
-
-ll findMinimumResult(const string &s)
-{
-    if (s.length() <= 2)
-    {
-        // Return the original value
-        ll val = 0;
-        for (char c : s)
-        {
-            val = val * 10 + (c - '0');
-        }
-        return val;
-    }
-    // convert s to an set
-    set<char> st;
-    for (char c : s)
-    {
-        st.insert(c);
-    }
-    if (st.size() == 1)
-    {
-        return s[0] - '0';
-    }
-    else if (st.size() == 2)
-    {
-        if (st.find(1) != st.end() and st.find(0) != st.end())
-        {
-            return 1;
-        }
-    }
-    if (s.size() == 3)
-    {
-        int sum = 0;
-        // get the sum of non-one numbers
-        for (char c : s)
-        {
-            if (c != '1')
-            {
-                sum += c - '0';
-            }
-        }
-        return sum;
-    }
-    else if (s.size() > 3 && !containsZero(s))
-    {
-        // choose the smallest two number from s, and the mark the location visited
-        int n = s.size();
-        int first = -1, second = -1;
-        vector<bool> visited(n);
-        int minValue = INT_MAX;
-        for (int i = 0; i < n - 1; i++)
-        {
-            int current_value = (s[i] - '0') * 10 + (s[i + 1] - '0');
-            if (minValue > current_value && current_value != 11)
-            {
-                minValue = current_value;
-                first = i;
-                second = i + 1;
-            }
-        }
-        visited[first] = true;
-        visited[second] = true;
-        // now get the sum of rest of the digits, ignoring the
-        // places where we have one, and the place where we have visited digits
-        int current_sum = 0;
-        for (int i = 0; i < s.size(); i++)
-        {
-            if (visited[i] == false && s[i] != '1')
-            {
-                current_sum += s[i] - '0';
-            }
-        }
-        return current_sum + minValue;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t;
+    ll t;
     cin >> t;
-
     while (t--)
     {
-        int n;
-        string s;
-        cin >> n >> s;
-        s = trimZero(s);
-        ll result = findMinimumResult(s);
-        cout << result << "\n";
+        ll n;
+        cin >> n;
+        char s[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> s[i];
+        }
+        if (n < 3)
+        {
+            cout << atoi(s) << endl;
+        }
+        else
+        {
+            // check if it contains Zero
+            bool containsZero = false;
+            for (auto i : s)
+            {
+                if (i == '0')
+                {
+                    containsZero = true;
+                    break;
+                }
+            }
+            if (containsZero == true)
+            {
+                cout << "0\n";
+            }
+            else
+            {
+                // take two digits as numeral,and other non-one numbers
+                int mini = 0;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    int current_sum = 0;
+                    int numeral = (s[i] - '0') * 10 + (s[i + 1] - '0');
+                    current_sum += numeral;
+                    for (int j = i + 3; j < n; j++)
+                    {
+                        if (s[j] != '1')
+                        {
+                            current_sum += (s[j] - '0');
+                        }
+                    }
+                    mini = min(current_sum, mini);
+                }
+                cout << mini << endl;
+            }
+        }
     }
-
     return 0;
 }
