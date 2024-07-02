6bf8c992c4d1385771512b6bcfc74e2584f68e7f
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-07-02 18:19:55 +0530

    Advent of code day 4 task 2 done

--- advent_of_code/2023/day4/2.go
+++ advent_of_code/2023/day4/2.go
@@ -1,50 +1,80 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
+// Extracts the ID of the card from the input string
+func getId(input string) int {
+	idxD := strings.Index(input, "d")
+	idxColon := strings.Index(input, ":")
+	s := input[idxD+2 : idxColon]
+	s = strings.TrimSpace(s)
+	num, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatalf("Failed to convert string to integer: %v", err)
+	}
+	return num
+}
+
+// Counts the number of matches between the winning numbers and the actual numbers
+func getCount(input string) int {
+	idxColon := strings.Index(input, ":")
+	idxDash := strings.Index(input, "|")
+	winningPart := input[idxColon+1 : idxDash]
+	actualPart := input[idxDash+1:]
+
+	winningNumbers := make(map[string]bool)
+	for _, num := range strings.Fields(winningPart) {
+		winningNumbers[num] = true
+	}
+
+	count := 0
+	for _, num := range strings.Fields(actualPart) {
+		if winningNumbers[num] {
+			count++
+		}
+	}
+	return count
+}
+
 func main() {
-	writer := bufio.NewWriter(os.Stdout)
-	reader := bufio.NewReader(os.Stdin)
-	writer.WriteString("Write relative path to the input file:- ")
-	writer.Flush()
-	path, _ := reader.ReadString('\n')
-	path = strings.TrimSpace(path)
-	data, _ := os.ReadFile(path)
+	//reader := bufio.NewReader(os.Stdin)
+	//fmt.Print("Write relative path to the input file: ")
+	//path, err := reader.ReadString('\n')
+	//if err != nil {
+	//	log.Fatalf("Failed to read input: %v", err)
+	//}
+	//path = strings.TrimSpace(path)
+
+	data, err := os.ReadFile("./1.txt")
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
 	dataString := string(data)
 	inputs := strings.Split(dataString, "\n")
+
+	freqHashMap := make(map[int]int)
+	for _, input := range inputs {
+		if len(input) > 1 {
+			id := getId(input)
+			freqHashMap[id]++
+		}
+	}
 
 	sum := 0
 	for _, input := range inputs {
 		if len(input) > 1 {
-			idxColor := strings.Index(input, ":")
-			idxDash := strings.Index(input, "|")
-			winningPart := input[idxColor+1 : idxDash]
-			actualPart := input[idxDash+1:]
-			//printing the winning numbers
-			numbers := strings.Split(winningPart, " ")
-			winningNumbers := make(map[string]bool)
-			for _, num := range numbers {
-				if num != "" {
-					winningNumbers[num] = true
-				}
+			id := getId(input)
+			count := getCount(input)
+			sum += freqHashMap[id]
+			for i := 1; i <= count; i++ {
+				freqHashMap[id+i] += freqHashMap[id]
 			}
-
-			actualNumbers := strings.Split(actualPart, " ")
-			count := 0
-			for _, num := range actualNumbers {
-				if num != "" && winningNumbers[num] {
-					count++
-				}
-			}
-			if count > 0 {
-				sum += (1 << (count - 1))
-			}
-
 		}
 	}
 	fmt.Printf("Sum = %v\n", sum)
---
+++ advent_of_code/2023/day4/day4.py
@@ -0,0 +1,34 @@
+import re
+
+with open('./1.txt', 'r') as f:
+    puzzle_input = f.read()
+
+
+def part1(puzzle_input):
+    regex = r':(.*)\|(.*)'
+    points = 0
+    for win_nums, true_nums in re.findall(regex, puzzle_input):
+        overlap = set(win_nums.split()) & set(true_nums.split())
+        if overlap:
+            points += 2 ** (len(overlap) - 1)
+
+    return points
+
+
+
+def part2(puzzle_input):
+    lines = puzzle_input.split('\n')
+    regex = r':(.*)\|(.*)'
+    cards = [1] * len(lines)
+    for i, line in enumerate(lines):
+        win_nums, actual_nums = re.findall(regex, line)[0]
+        overlap = set(win_nums.split()) & set(actual_nums.split())
+        for j in range(len(overlap)):
+            cards[i+j+1] += cards[i]
+    
+    return sum(cards)
+
+
+
+print('Part 1:', part1(puzzle_input))
+print('Part 2:', part2(puzzle_input))
