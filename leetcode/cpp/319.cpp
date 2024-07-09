039bb44093d22af68f540a0bc0a9831e02d36ad5
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-07-10 02:11:46 +0530

    bulb switcher

---
+++ leetcode/cpp/319.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        map<int,int> mp;
+        for(int i = 1; i <= )        
+    }
+};
