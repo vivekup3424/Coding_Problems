06b34f4a4627073ab863aa322b0b89946777967f
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-07-10 03:39:05 +0530

    saving the recursion code

--- leetcode/cpp/319.cpp
+++ leetcode/cpp/319.cpp
@@ -6,6 +6,9 @@ class Solution {
 public:
     int bulbSwitch(int n) {
         map<int,int> mp;
-        for(int i = 1; i <= )        
+        for(int i = 1; i <= sqrt(n); i++){
+            int temp = n;
+            if
+        }        
     }
 };
---
+++ leetcode/cpp/552.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+//first let's try writing the recusive approach
+//we can have 3 states for each day
+//1. Present
+//2. Absent
+//3. Late
+
+//we can have 2 variables to keep track of the number of days we have been absent and the number of days we have been late consecutively
+void recurse(int n, int absent, int late, int i, int &count){
+    if(i==n){
+        if(absent<=1 && late<=2){
+            count++;
+        }
+    }
+    else{
+        recurse(n,absent+1,0,i+1,count); //getting absent today
+        recurse(n,absent,late+1,i+1,count); //getting late today
+        recurse(n,absent,0,i+1,count); //getting present today
+    }
+}
+//building upon the recursive solution, I can make a memoization from it
+//for this I need to think of its dimension
+//clearly I can think of making it a 3D-dp, but let's see
+
+class Solution {
+public:
+    int checkRecord(int n) {
+        int count = 0;
+        recurse(n,0,0,0,count);
+        return count;
+    }
+}
