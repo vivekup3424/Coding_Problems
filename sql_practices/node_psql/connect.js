4440abc24e7d8a7e60409d51ef029083f1a1ccbb
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-07-09 10:51:35 +0530

    update

--- sql_practices/node_psql/connect.js
+++ sql_practices/node_psql/connect.js
@@ -3,8 +3,8 @@ const pg = require("pg");
 const pool = new pg.Pool({
   user: "postgres",
   host: "localhost",
-  database: "postgres",
-  password: "lol",
+  database: "recipes",
+  password: "postgres",
   port: 5432,
 });
 async function fetchData() {
@@ -13,5 +13,4 @@ async function fetchData() {
   console.log(rows);
   return result;
 }
-const result = fetchData();
-console.log(result);
+await fetchData();
