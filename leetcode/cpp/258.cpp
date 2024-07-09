38991bc18ba3921a7ddefb85f7a495eda3a92b17
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-07-10 01:49:49 +0530

    add digits

--- leetcode/cpp/258.cpp
+++ leetcode/cpp/258.cpp
@@ -21,6 +21,17 @@ public:
             return addDigits(n);
         }
     }
+    int addDigits2(int num){
+        if(num == 0){
+            return 0;
+        }
+        else if(num % 9 == 0){
+            return 9;
+        }
+        else{
+            return num % 9;
+        }
+    }
 };
 int main()
 {
