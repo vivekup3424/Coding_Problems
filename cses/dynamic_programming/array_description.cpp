9fb348f06fec176eaa6b0a0d687f9a073d60242d
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-07-10 13:15:56 +0530

    removed extra binaries

--- cses/dynamic_programming/array_description.cpp
+++ cses/dynamic_programming/array_description.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int MOD = 1e9 + 7;
 int recurse(vector<int> a, int n, int m, int i)
 {
     if (i == 0)
@@ -43,6 +44,39 @@ int main(int argc, char const *argv[])
 {
     int n, m;
     cin >> n >> m;
+    vector<vector<int>> dp(n, vector<int>(m + 1, 0));
+    int x0;
+    cin >> x0;
+    if (x0 == 0)
+    {
+        fill(dp[0].begin(), dp[0].end(), 1);
+    }
+    else
+    {
+        dp[0][x0] = 1;
+    }
+    for (int i = 1; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        if (x == 0)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                for (int k : {j - 1, j, j + 1})
+                {
+                    if (k >= 1 && k <= m)
+                    {
+                        (dp[i][j] += dp[i - 1][j]) % MOD;
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (int)
+        }
+    }
     vector<int> a(n);
     for (int i = 0; i < n; i++)
     {
