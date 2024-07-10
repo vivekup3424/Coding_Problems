bc7b2ce25b8fba7d533a9005bde059ae56294b37
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-07-10 06:26:54 +0530

    did some refactoring for minimizing coins

--- cses/dynamic_programming/coin_combinations_1.cpp
+++ cses/dynamic_programming/coin_combinations_1.cpp
@@ -4,14 +4,7 @@ typedef long long ll;
 typedef long long unsigned llu;
 typedef std::vector<int> vi;
 typedef std::vector<long long> vll;
-const int MOD = 1e9+7;
-
-inline void fast_io()
-{
-    ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    cout.tie(NULL);
-}
+const int MOD = 1e9 + 7;
 
 // Template function to input elements to a vector
 template <typename T>
@@ -34,63 +27,78 @@ void outputVector(const vector<T> &v, int n)
     }
     cout << "\n";
 }
-//let me write the recursive code first
-//draw the decision tree, you will get
-//the intuition from there
-int total_ways(vll coins,int sum){
-    if(sum==0)return 1;
-    else{
+// let me write the recursive code first
+// draw the decision tree, you will get
+// the intuition from there
+int total_ways(vll coins, int sum)
+{
+    if (sum == 0)
+        return 1;
+    else
+    {
         ll total = 0;
         for (int i = 0; i < coins.size(); i++)
         {
-            if(coins[i]<=sum){
+            if (coins[i] <= sum)
+            {
                 total += total_ways(coins, sum - coins[i]);
             }
         }
         return total;
     }
 }
-//now I write the memoized code
-//since this is a difficult question for me,
-//I have to do this long process
-ll total_ways_memo(vll coins, vll memo, ll sum){
-    if(memo[sum]!=-1)return memo[sum];
-    else if(sum==0)return memo[sum] = 1;
-    else{
+// now I write the memoized code
+// since this is a difficult question for me,
+// I have to do this long process
+ll total_ways_memo(vll coins, vll memo, ll sum)
+{
+    if (memo[sum] != -1)
+        return memo[sum];
+    else if (sum == 0)
+        return memo[sum] = 1;
+    else
+    {
         ll total = 0;
         for (int i = 0; i < coins.size(); i++)
         {
-            if(coins[i]<=sum){
-                total =(total + total_ways(coins, sum - coins[i]))%MOD;
+            if (coins[i] <= sum)
+            {
+                total = (total + total_ways(coins, sum - coins[i])) % MOD;
             }
         }
         return memo[sum] = total;
     }
 }
-//well let's try the dp way
+// well let's try the dp way
 int main()
 {
-    fast_io();
-    ll T=1;
-    while (T--)
-    {
-	ll n,x;
-	cin>>n>>x;
+    ll n, x;
+    cin >> n >> x;
     vll coins(n);
-    inputVector(coins,n);
+    inputVector(coins, n);
     sort(coins.begin(), coins.end());
-    vll dp(x+1,0);
-    dp[0] = 1;
-    for (int i = 1; i < n+1; i++)
+    vector<vector<ll>> dp(n + 1, vector<ll>(x + 1, 0));
+    for (int i = 0; i <= n; i++)
     {
-        for (int j = 0; j < x+1; j++)
+        for (int j = 0; j <= x; j++)
         {
-            if(coins[i-1]<=j){
-                dp[j] = dp[j] + dp[j - coins[i-1]]%MOD;
+            if (i == 0)
+            {
+                dp[i][j] = 0;
+            }
+            else if (j == 0)
+            {
+                dp[i][j] = 1;
+            }
+            else if (coins[i - 1] <= j)
+            {
+                dp[i][j] += (dp[i][j - coins[i - 1]] + dp[i - 1][j]) % MOD;
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j] % MOD;
             }
         }
     }
-    
-    cout<<dp[x]<<endl;
-    }
+    cout << dp[n][x] << "\n";
 }
--- cses/dynamic_programming/minimizing_coins.cpp
+++ cses/dynamic_programming/minimizing_coins.cpp
@@ -1,61 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef long long unsigned llu;
-typedef std::vector<int> vi;
-typedef std::vector<long long> vll;
-const int MOD = 1e9+7;
-
-inline void fast_io()
-{
-    ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    cout.tie(NULL);
-}
-
-// Template function to input elements to a vector
-template <typename T>
-void inputVector(vector<T> &v, int n)
-{
-    v.resize(n);
-    for (int i = 0; i < n; i++)
-    {
-        std::cin >> v[i];
-    }
-}
-
-// Template function to output elements of a vector
-template <typename T>
-void outputVector(const vector<T> &v, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cout << v[i] << " ";
-    }
-    cout << "\n";
-}
-
 int main()
 {
-    fast_io();
-    ll T=1;
-    while (T--)
+    int n, x;
+    cin >> n >> x;
+    vector<int> c(n);
+    for (int i = 0; i < n; i++)
     {
-	ll n,x;
-	cin>>n>>x;
-    vll coins(n);
-    inputVector(coins,n);
-    vll dp(x+1,INT_MAX);
-    for (int i = 1; i < n+1; i++)
-    {       
-        for (int j = 0; j < x+1; j++)
+        cin >> c[i];
+    }
+    vector<int> dp(x + 1, INT_MAX);
+    dp[0] = 0;
+    for (int i = 1; i <= x; i++)
+    {
+        for (int j = 0; j < n; j++)
         {
-            if(j==0)dp[j] = 0;
-            else if(coins[i-1]<=j){
-                dp[j] = min(dp[j], 1 + dp[j-coins[i-1]]);
+            if (i - c[j] >= 0)
+            {
+                dp[i] = min(dp[i], dp[i - c[j]] + 1);
             }
         }
     }
-        cout<<(dp[x]==INT_MAX ? -1 : dp[x])<<endl;
-    }
+    cout << (dp[x] == INT_MAX ? -1 : dp[x]) << endl;
 }
