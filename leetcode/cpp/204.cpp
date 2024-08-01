0d1f0aa41a3112d7d8ad789b1e477b6162a87ed2
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-01 22:43:34 +0530

    count primes question done

---
+++ leetcode/cpp/204.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int countPrimes(int n)
+    {
+        vector<bool> isPrimes(n + 1, true);
+        int count = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            if (isPrimes[i])
+            {
+                count++;
+                for (int j = i * i; j <= n; j += i)
+                {
+                    isPrimes[j] = false;
+                }
+            }
+        }
+        return count;
+    }
+};
