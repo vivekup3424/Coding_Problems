5e40877d0b95dfa6e8f2ec265114e0bf9ef510a5
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-01 22:01:20 +0530

    update

--- leetcode/cpp/1105.cpp
+++ leetcode/cpp/1105.cpp
@@ -1,17 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
+f(i,remainingShelfWidth,maxHeight)=min(maxHeight+f(i+1,shelfWidth−books[i][0],books[i][1]),f(i+1,remainingShelfWidth−books[i][0],max(maxHeight,books[i][1])))
+*/
 class Solution
 {
 public:
+    int recurse(vector<vector<int>> &books, int shelfWidth, int i, int remainingShelfWidth, int maxHeight)
+    {
+        if (i == books.size() - 1)
+        {
+            if (remainingShelfWidth - books[i - 1][0] < 0)
+            {
+                return maxHeight + books[i][1];
+            }
+            else
+            {
+                return max(maxHeight, books[i][0]);
+            }
+        }
+        else
+        {
+            return min(
+                maxHeight + recurse(books, shelfWidth, i + 1, shelfWidth - books[i][0], books[i][1]),
+                recurse(books, shelfWidth, i + 1, remainingShelfWidth - books[i][0], max(maxHeight, books[i][1])));
+        }
+    }
+    int bottomup(vector<vector<int>> &books, int sheldWidth)
+    {
+        int n = books.size();
+        int dp[n];
+        dp[0] = books[i] for (int i = 0; i < n; i++)
+        {
+        }
+    }
     int minHeightShelves(vector<vector<int>> &books, int shelfWidth)
     {
-        sort(books.begin(), books.end(), [](const vector<int> &a, const vector<int> &b)
-             { return a[1] > b[1]; });
-        // output the vectors
-        for (auto book : books)
-        {
-            cout << book[0] << ", " << book[1] << endl;
-        }
-        return 6;
     }
 };
