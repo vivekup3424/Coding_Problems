8c80d9548db57769054012b81df113eaa1979367
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-01 22:44:08 +0530

    small correction done here

--- leetcode/cpp/204.cpp
+++ leetcode/cpp/204.cpp
@@ -8,7 +8,7 @@ public:
     {
         vector<bool> isPrimes(n + 1, true);
         int count = 0;
-        for (int i = 2; i <= n; i++)
+        for (int i = 2; i < n; i++)
         {
             if (isPrimes[i])
             {
