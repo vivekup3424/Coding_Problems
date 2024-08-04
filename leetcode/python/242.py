133a2f17d9f0806a6e19ae7601e29764d7dad638
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-04 15:55:10 +0530

    ancient dragon man

--- leetcode/python/242.py
+++ leetcode/python/242.py
@@ -1,4 +1,12 @@
-import string
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) ==sorted(t)
+        temp1 = sorted(s)
+        temp2 = sorted(t)
+        print(temp1)
+        print(temp2)
+        if(len(temp1) != len(temp2)):
+            return False
+        for i in range(len(temp1)):
+            if(temp1[i] != temp2[i]):
+                return False
+        return True
