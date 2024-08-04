b539d46a0859742711467ae5d0988527915fdc9d
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-04 19:29:21 +0530

    meeting schedule done

---
+++ leetcode/cpp/meeting_schedule.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Interval
+{
+public:
+    int start, end;
+    Interval(int start, int end)
+    {
+        this->start = start;
+        this->end = end;
+    }
+};
+
+class Solution
+{
+public:
+    bool canAttendMeetings(vector<Interval> &intervals)
+    {
+        sort(intervals.begin(), intervals.end(), [](const Interval &a, const Interval &b)
+             { return a.start < b.start; });
+        for (int i = 1; i < intervals.size(); ++i)
+        {
+            if (intervals[i].start < intervals[i - 1].end)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
