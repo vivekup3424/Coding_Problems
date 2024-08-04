d1f86cf3ffcee8cc653b07dcde70f7d6685e7d55
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-04 16:59:47 +0530

    leetcode 238 Products of Array Discluding Self done

--- leetcode/python/271.py
+++ leetcode/python/271.py
@@ -7,3 +7,16 @@ class Solution:
             ans += str(len(s)) + "#" + s
         return ans
     def decode(self, s: str) -> List[str]:
+        ans = []
+        i = 0
+        while i < len(s):
+            j = i
+            #getting the integer(length of the string)
+            while s[j]!='#':
+                j+=1
+            length = int(s[i:j])
+            i = j + 1 #skipping over the '#'
+            ans.append(s[i:i+length])
+            j = i+length
+            i = j
+        return ans
