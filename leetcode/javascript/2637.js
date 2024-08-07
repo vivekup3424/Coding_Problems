01d315ee9eea8cf930ac80337a75cb976cf83df7
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-07 17:28:28 +0530

    update

--- leetcode/javascript/2637.js
+++ leetcode/javascript/2637.js
@@ -3,19 +3,32 @@
  * @param {number} t
  * @return {Function}
  */
+//var timeLimit = function (fn, t) {
+//  return async function (...args) {
+//    return new Promise((resolve, reject) => {
+//      setTimeout(() => {
+//        return reject("Time Limit Exceeded");
+//      }, t);
+//      fn(...args)
+//        .then((response) => resolve(response))
+//        .catch((err) => reject(err));
+//    });
+//  };
+//};
+
 var timeLimit = function (fn, t) {
   return async function (...args) {
     return new Promise((resolve, reject) => {
-      setTimeout(() => {
+      const id = setTimeout(() => {
         return reject("Time Limit Exceeded");
       }, t);
       fn(...args)
         .then((response) => resolve(response))
-        .catch((err) => reject(err));
+        .catch((err) => reject(err))
+        .finally(() => clearTimeout(id));
     });
   };
 };
-
 /**
  * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
  * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
