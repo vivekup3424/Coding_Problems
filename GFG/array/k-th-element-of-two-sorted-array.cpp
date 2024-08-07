ec8154ba3022d170337ccedba6914c2af9185534
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-07 20:34:44 +0530

    kahaan hu main

--- GFG/array/k-th-element-of-two-sorted-array.cpp
+++ GFG/array/k-th-element-of-two-sorted-array.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 // Method 1
 /*
-[Naive Approach] Merge Two Sorted Arrays using Sorting – O((m + n) * log(m + n)) time and O(m + n) auxiliary space
+[Naive Approach] Merge Two Sorted Arrays using Sorting – O(m + n) time and O(m + n) auxiliary space:
 */
 
 int kth_sorted(int arr1[], int arr2[], int k)
@@ -35,4 +35,64 @@ int kth_sorted(int arr1[], int arr2[], int k)
         sorted1[d++] = arr2[j++];
     }
     return sorted1[k - 1];
+}
+
+// Method 2
+/*
+[Better approach] Space Optimized Two pointers Merge Approach –
+O(k) time and
+O(1) auxiliary space:
+*/
+/*
+This approach optimizes the space complexity of the
+above naive approach by avoiding the creation of
+an additional array. Instead, we use two pointers
+to traverse the input arrays and count the elements
+until we reach the k-th element.
+*/
+int kth_two_pointers(int arr1[], int arr2[], int k)
+{
+    int n = sizeof(arr1) / sizeof(int);
+    int m = sizeof(arr2) / sizeof(int);
+    int d = 0, i = 0, j = 0;
+    while (i < n and j < m)
+    {
+        if (arr1[i] < arr2[j])
+        {
+            d++;
+            if (d == k)
+            {
+                return arr1[i];
+            }
+            i++;
+        }
+        else
+        {
+            d++;
+            if (d == k)
+            {
+                return arr2[j];
+            }
+            j++;
+        }
+    }
+    while (i < m)
+    {
+        d++;
+        if (d == k)
+        {
+            return arr1[i];
+        }
+        i++;
+    }
+    while (j < n)
+    {
+        d++;
+        if (d == k)
+        {
+            return arr2[j]++;
+        }
+        j++;
+    }
+    return -1;
 }
