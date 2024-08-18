0623617983811836077a131b2830990142794751
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-18 17:04:03 +0530

    some kill

--- leetcode/cpp/757.cpp
+++ leetcode/cpp/757.cpp
@@ -6,5 +6,11 @@ class Solution
 public:
     int intersectionSizeTwo(vector<vector<int>> &intervals)
     {
+        sort(intervals.begin(), intervals.end(), [](vector<int> &a, vector<int> &b)
+             {
+            if(a[1]==b[1]){
+                return a[0] > b[0];
+            }
+            return a[1] < b[1]; });
     }
 };
