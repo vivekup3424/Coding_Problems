722de0692c84fd7305bd5335c2c7c108c06e1d99
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-18 15:56:39 +0530

    B done

--- leetcode/cpp/3259.cpp
+++ leetcode/cpp/3259.cpp
@@ -1,36 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution
+long long maxEnergyBoost(vector<int> &A, vector<int> &B)
 {
-public:
-    long long maxEnergyBoost(vector<int> &energyDrinkA, vector<int> &energyDrinkB)
+    long long a0 = 0, a1 = 0, a2 = 0, b0 = 0, b1 = 0, b2 = 0, n = A.size();
+    for (int i = 0; i < n; i++)
     {
-        int n = energyDrinkA.size();
-        vector<long long> pref_A(n, 0);
-        vector<long long> pref_B(n, 0);
-        vector<long long> suff_A(n, 0);
-        vector<long long> suff_B(n, 0);
-        for (int i = 1; i < n; i++)
-        {
-            pref_A[i] = pref_A[i - 1] + energyDrinkA[i - 1];
-            pref_B[i] = pref_B[i - 1] + energyDrinkB[i - 1];
-        }
-        for (int i = n - 2; i >= 0; i--)
-        {
-            suff_A[i] = suff_A[i + 1] + energyDrinkA[i + 1];
-            suff_B[i] = suff_B[i + 1] + energyDrinkB[i + 1];
-        }
-        long long maxi = 0;
-        for (int i = 0; i < n; i++)
-        {
-            long long c = max({
-                pref_A[i] + suff_A[i] + energyDrinkA[i],
-                pref_B[i] + suff_B[i] + energyDrinkB[i],
-                pref_A[i] + suff_B[i],
-                pref_B[i] + suff_A[i],
-            });
-            maxi = max(maxi, c);
-        }
-        return maxi;
+        a2 = A[i] + max(a1, b0);
+        b2 = B[i] + max(b1, a0);
+        a0 = a1;
+        a1 = a2;
+        b0 = b1;
+        b1 = b2;
     }
-};
+    return max(a1, b1);
+}
