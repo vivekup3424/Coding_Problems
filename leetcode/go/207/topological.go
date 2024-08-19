eb386b172724d6a4bb5f03563a1f614cc64ad775
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-20 04:10:58 +0530

    lougout of them

--- leetcode/go/207/topological.go
+++ leetcode/go/207/topological.go
@@ -1,26 +1,4 @@
 package main
 
-import "fmt"
-
 func canFinishTopo(prerequisities [][]int, numCourses int) bool {
-	graph := make([][]int, numCourses)
-	for i := 0; i < numCourses; i++ {
-		graph[i] = make([]int, 0)
-	}
-	for _, pre := range prerequisities {
-		a := pre[0]
-		b := pre[1]
-		graph[a] = append(graph[a], b)
-	}
-	fmt.Println("Graph = ", graph)
-	indegree := make([]int, numCourses)
-	for i := 0; i < numCourses; i++ {
-		for _, node := range graph[i] {
-			indegree[node]++
-		}
-	}
-	fmt.Println("Indegrees = ", indegree)
-	// doing topological sort
-
-	return false
 }
