9a6d2d4872e0afc4183de360bd1399021e5dff39
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-20 03:10:40 +0530

    slight save

--- leetcode/go/1.go
+++ leetcode/go/1.go
@@ -13,3 +13,21 @@ func twoSum(nums []int, target int) []int {
 	}
 	return nil
 }
+
+func twoSumsAgain(nums []int, target) []int {
+	mp := make(map[int]int) //stores element-index
+	for idx,val := range nums{
+		elem2 := target - val
+		if idx2, exists := mp[elem2]; exists{
+			return []int{idx,idx2}
+		}
+	}
+	return nil
+}
+
+func twoSumsFor(nums []int, target) []int{
+	for i := 0; i < len(nums); i++{
+		elem2 = target - nums[i]
+		for j := i + 1; j <
+	}
+}
