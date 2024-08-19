b46fd1d9ef25610380b70d861983e9d89f673ddc
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-20 03:26:25 +0530

    make me quiet please

--- leetcode/go/36.go
+++ leetcode/go/36.go
@@ -1,15 +1,21 @@
 package main
 
-import "go/format"
+import "slices"
 
-type coordinate struct{
+type coordinate struct {
 	x int
 	y int
 }
+
 func isValidSudoku(board [][]byte) bool {
-	row_map := map[int][]int{}
-	col_map := map[int][]int{}
-	box_map := map[coordinate][]int{}
-	for i := 0;i < 
-	return false
+	row_map := map[int][]byte{}
+	col_map := map[int][]byte{}
+	box_map := map[coordinate][]byte{}
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			val := board[i][j]
+			if slices.Contains(row_map[i],val)
+		}
+	}
+	return true
 }
