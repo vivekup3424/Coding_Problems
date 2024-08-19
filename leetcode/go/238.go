55357588a6810f1ac1a022e63d0d098440be6346
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-20 03:42:18 +0530

    fallen creature

--- leetcode/go/238.go
+++ leetcode/go/238.go
@@ -1,1 +1,20 @@
 package main
+
+func productExceptSelf(nums []int) []int {
+	n := len(nums)
+	res := make([]int, n)
+
+	preProduct := 1
+	for i := 0; i < n; i++ {
+		res[i] = preProduct
+		preProduct *= nums[i]
+	}
+
+	sufProduct := 1
+	for i := n - 1; i >= 0; i-- {
+		res[i] *= sufProduct
+		sufProduct *= nums[i]
+	}
+
+	return res
+}
