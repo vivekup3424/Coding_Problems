f4bed7b71ab09b258fa62b68c17411405f4b7bd4
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-25 21:11:20 +0530

    i need help

---
+++ GFG/searching/binarySearch/number-of-pairs.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long countPairs(vector<int> &arr, vector<int> &brr)
+{
+    int n = arr.size();
+    int m = brr.size();
+    sort(brr.begin(), brr.end());
+    long long count = 0;
+}
