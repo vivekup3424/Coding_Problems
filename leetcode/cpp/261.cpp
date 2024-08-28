b2503ec1d709bf25c7737a19b07e396440145f65
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-28 12:09:12 +0530

    push

--- leetcode/cpp/261.cpp
+++ leetcode/cpp/261.cpp
@@ -6,5 +6,14 @@ class Solution
 public:
     bool validTree(int n, vector<vector<int>> &edges)
     {
+        if (edges.size() != n - 1)
+        {
+            return false;
+        }
+        vector<vector<int>> graph(n);
+        for (auto edge : edges)
+        {
+            graph[edge[0]].push_back(edge[1]);
+        }
     }
 };
