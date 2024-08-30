f412eab5ff059936b2270def4661a47f1152a8f2
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-08-30 23:40:06 +0530

    p

---
+++ GFG/backtracking/nqueen.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void recurse(set<int> occupied, )
+        vector<vector<int>> nQueen(int n)
+    {
+        // code here
+    }
+};
