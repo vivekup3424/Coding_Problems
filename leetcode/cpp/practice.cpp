95a70b8cf9e53c36516f728a3879c23515b33b8b
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-09-01 19:32:02 +0530

    skmc

--- leetcode/cpp/practice.cpp
+++ leetcode/cpp/practice.cpp
@@ -1,55 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-class DSU
+
+class Solution
 {
 public:
-    int vertices;
-    std::vector<int> parent;
-    std::vector<int> rank;
-
-    // Constructor initializes the DSU with n vertices
-    DSU(int n)
+    int maxScore(vector<vector<int>> &grid)
     {
-        vertices = n;
-        parent.resize(n);
-        rank.resize(n);
-        for (int i = 0; i < n; ++i)
+        map<int, priority_queue<int>> mp;
+        for (auto e : grid)
         {
-            parent[i] = i;
-            rank[i] = 0;
-        }
-    }
-
-    // Find function with path compression
-    int find(int u)
-    {
-        if (parent[u] != u)
-        {
-            parent[u] = find(parent[u]);
-        }
-        return parent[u];
-    }
-
-    // Union function with union by rank
-    void union_sets(int u, int v)
-    {
-        int root_u = find(u);
-        int root_v = find(v);
-
-        if (root_u != root_v)
-        {
-            if (rank[root_u] < rank[root_v])
+            priority_queue<int> p;
+            for (auto i : e)
             {
-                parent[root_u] = root_v;
-            }
-            else if (rank[root_u] > rank[root_v])
-            {
-                parent[root_v] = root_u;
-            }
-            else
-            {
-                parent[root_v] = root_u;
-                rank[root_u]++;
+                p.push(i);
             }
         }
     }
