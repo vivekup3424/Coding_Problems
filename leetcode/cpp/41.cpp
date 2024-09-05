e083159c34f7718779515c49c7d4a2f78615d9db
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-09-05 19:07:04 +0530

    Add debugging comments and stub for correct solution in

--- leetcode/cpp/41.cpp
+++ leetcode/cpp/41.cpp
@@ -48,8 +48,20 @@ public:
             }
         }
         return n + 1;
+        // this was the code, I could come up with this much of the solution
+        // by myself,
+        // I am failing at testcase where there are repeated numbers
+        // nums =
+        //[-3,9,16,4,5,16,-4,9,26,2,1,19,-1,25,7,22,2,-7,14,2,5,-6,1,17,3,24,-4,17,15]
     }
 };
+int correctSolution(vector<int> &nums)
+{
+    int n = nums.size();
+    // using the orignal vector nums as hashset
+    // for storing the numbers, and check the smallest missing positive number
+    // since we know that smallest positive number can be at max n+1
+}
 int firstMissingPositiveSort(vector<int> nums)
 {
     sort(nums.begin(), nums.end());
