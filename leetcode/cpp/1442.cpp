1b58ee0ed32df3f57d6897e40c6ee24c7a363074
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-09-09 20:51:03 +0530

    saved up

---
+++ leetcode/cpp/1442.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) {
+        int score = 0;
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
+            
+        }
+        return score;
+    }
+};
