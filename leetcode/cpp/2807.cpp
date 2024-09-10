0487feb1ea5170bbe0f8df3a2270d825aa937a3d
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-09-10 06:03:51 +0530

    2807. Insert Greatest Common Divisors in Linked List

--- leetcode/cpp/2807.cpp
+++ leetcode/cpp/2807.cpp
@@ -1,13 +1,13 @@
-#include <bits/stdc++.h>
-using namespace std;
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     
@@ -15,7 +15,7 @@ public:
        ListNode* temp = head;
        while(temp->next!=NULL)
        {
-        ListNode tempNext = temp->next;
+        ListNode *tempNext = temp->next;
         int a = temp->val;
         int b = temp->next->val;
         int c = __gcd(a,b);
@@ -23,13 +23,7 @@ public:
         newNode->next = tempNext;
         temp->next = newNode;
         temp = tempNext;
-       } 
+       }
+       return head; 
     }
 };
-int main()
-{
-    int a,b;
-    cin >> a >> b;
-    // get their gcd
-    int c = __gcd(a,b);
-}
