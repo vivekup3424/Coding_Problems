7c4115dd12c7c7d940ff08c9972e752302af19dc
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-09-16 23:20:54 +0530

    saveup your noodle

--- GFG/stacks/longest-valid-parenthesis.cpp
+++ GFG/stacks/longest-valid-parenthesis.cpp
@@ -12,45 +12,30 @@ using namespace std;
 class Solution {
 public:
   int maxLength(string s) {
-    stack<int> stk;
-    int maxi = 0;
-    for (char c : s) {
-      if (c == '(') {
-        stk.push(-1);
-      } else if (c == ')') {
-        int temp = 0;
-        while (stk.size() > 0 && stk.top() != 0 && stk.top() != -1) {
-          auto i = stk.top();
-          stk.pop();
-          temp += i;
+    stack<int> st;
+    st.push(-1);
+    int max_len = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+      if (s[i] == '(') {
+        st.push(i);
+      } else {
+        st.pop();
+        if (st.empty()) {
+          st.push(i);
+        } else {
+          max_len = max(max_len, i - st.top());
         }
-        if (stk.size()>0 && stk.top() == -1) {
-          stk.pop();
-          temp += 2;
-        }
-        maxi = max(maxi,temp);
-        stk.push(temp);
       }
     }
-    //empty up the stack
-    while(stk.size()>0){
-      auto s = 0;
-      while(stk.size()>0 and stk.top()>0){
-        s += stk.top();
-        stk.pop();
-      }
-      if(stk.size()>0){
-        stk.pop();
-      }
-      maxi = max(maxi,s);
-    }
-    return maxi;
+
+    return max_len;
   }
 };
-int main (int argc, char *argv[]) {
-  freopen("~/Desktop/fileInput.txt",  "r", stdin);
+int main(int argc, char *argv[]) {
+  freopen("~/Desktop/fileInput.txt", "r", stdin);
   string s;
-  cin>>s;
-  cout<<Solution().maxLength(s);
+  cin >> s;
+  cout << Solution().maxLength(s);
   return 0;
 }
