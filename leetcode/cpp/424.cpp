5ae56a2cb0489a6ef563f9e6415249109def178b
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-10-06 22:59:16 +0530

    maximum substring with character replacement

--- leetcode/cpp/424.cpp
+++ leetcode/cpp/424.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
 class Solution
 {
 public:
@@ -11,21 +13,11 @@ public:
         for (int end = 0; end < n; end++)
         {
             counts[s[end] - 'A']++;
-            if (maxCharCount < counts[s[end] - 'A'])
-            {
-                maxCharCount = counts[s[end] - 'A'];
-            }
+            maxCharCount = max(maxCharCount, counts[s[end] - 'A']);
             while (end - start - maxCharCount + 1 > k)
             {
                 counts[s[start] - 'A']--;
                 start++;
-                for (int i = 0; i < 26; i++)
-                {
-                    if (maxCharCount < counts[i])
-                    {
-                        maxCharCount = counts[i];
-                    }
-                }
             }
             result = max(result, end - start + 1);
         }
