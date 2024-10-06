4099ac2f4f1531a5bc175a031b1187df31ed729b
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-10-07 01:37:34 +0530

    added the try-catch if stack is empty

--- leetcode/cpp/155.cpp
+++ leetcode/cpp/155.cpp
@@ -1,8 +1,60 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+class MinStack
 {
-    
-}
+    // Naive Approach
+public:
+    stack<pair<int, int>> stk;
+    MinStack()
+    {
+        ;
+    }
+
+    void push(int val)
+    {
+        if (stk.size() == 0)
+        {
+            stk.push({val, val});
+        }
+        else
+        {
+            int mini = min(val, stk.top().second);
+            stk.push({val, mini});
+        }
+    }
+
+    void pop()
+    {
+        if (stk.size() > 0)
+        {
+            stk.pop();
+        }
+    }
+
+    int top()
+    {
+        return stk.top().first;
+    }
+
+    int getMin()
+    {
+        try
+        {
+            stk.top().second;
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
