c325bf13ea574a7d50e22a965b2c1934b33f7ff9
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-10-09 22:59:44 +0530

    kth-largest-element-in-a-stream.cpp

---
+++ kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class KthLargest {
+public:
+    KthLargest(int k, vector<int>& nums) {
+        
+    }
+    
+    int add(int val) {
+        
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
