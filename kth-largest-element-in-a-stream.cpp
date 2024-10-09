ed9a3fd29c0cf0c01c8fafa4fb6ec0a78d91661f
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-10-09 23:21:12 +0530

    kth-largest-element-in-a-stream.cpp

--- kth-largest-element-in-a-stream.cpp
+++ kth-largest-element-in-a-stream.cpp
@@ -1,15 +1,30 @@
 #include <bits/stdc++.h>
+#include <queue>
 using namespace std;
 
 class KthLargest {
+private:
+  priority_queue<int, vector<int>, greater<int>> minHeap;
+  int heapSize;
+
 public:
-    KthLargest(int k, vector<int>& nums) {
-        
+  KthLargest(int k, vector<int> &nums) {
+        heapSize = k;
+    for (auto i : nums) {
+      minHeap.push(i);
+      if (minHeap.size() > k) {
+        minHeap.pop();
+      }
     }
-    
-    int add(int val) {
-        
+  }
+
+  int add(int val) {
+    minHeap.push(val);
+    if (minHeap.size() > heapSize) {
+      minHeap.pop();
     }
+    return minHeap.top();
+  }
 };
 
 /**
@@ -17,3 +32,7 @@ public:
  * KthLargest* obj = new KthLargest(k, nums);
  * int param_1 = obj->add(val);
  */
+int main(){
+    vector<int> v = {7,7,7,7,8,3};
+    auto A = new KthLargest(4,v);
+}
