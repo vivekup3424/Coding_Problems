374ade0f564d592e7c7507cd3a6bb9537c175f0b
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-10-09 23:58:31 +0530

    k-closest-points-to-origin

--- leetcode/cpp/973.cpp
+++ leetcode/cpp/973.cpp
@@ -1,15 +1,48 @@
+#include <bits/stdc++.h>
+#include <queue>
+using namespace std;
+
+class Node {
+public:
+  int distanceSquared;
+  int x;
+  int y;
+  Node(int d, int x, int y) {
+    this->distanceSquared = d;
+    this->x = x;
+    this->y = y;
+  }
+};
+class comparator {
+public:
+  bool operator()(Node* A, Node* B) {
+    return A->distanceSquared < B->distanceSquared;
+  }
+};
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<pair<int, int>> dist(points.size());
-        for (int i = 0; i < points.size(); ++i) {
-            dist[i] = {points[i][0] * points[i][0] + points[i][1] * points[i][1], i};
-        }
-        nth_element(dist.begin(), dist.begin() + k - 1, dist.end());
-        vector<vector<int>> result(k);
-        for (int i = 0; i < k; ++i) {
-            result[i] = move(points[dist[i].second]);
-        }
-        return result;
+  vector<vector<int>> kClosest(vector<vector<int>> &points, int k) {
+    int n = points.size();
+    priority_queue<Node*, vector<Node*>, comparator> maxHeap;
+    for (int i = 0; i < n; i++) {
+      int x = points[i][0], y = points[i][1];
+      int d = x * x + y * y;
+      auto A = new Node(d, x, y);
+      maxHeap.push(A);
     }
+    
+    // Remove elements to maintain top k elements in maxHeap
+    while (maxHeap.size() > k) {
+      maxHeap.pop();
+    }
+
+    vector<vector<int>> v;
+    while (maxHeap.size() > 0) {
+      auto A = maxHeap.top();
+      v.push_back({A->x, A->y});
+      maxHeap.pop();
+    }
+
+    return v;
+  }
 };
