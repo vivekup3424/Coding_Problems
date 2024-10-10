f82050993275ae171ba95f7f4b522e18a35778ed
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-10-10 20:10:26 +0530

    max-area-of-island.cpp

--- leetcode/cpp/max-area-of-island.cpp
+++ leetcode/cpp/max-area-of-island.cpp
@@ -3,33 +3,30 @@ using namespace std;
 
 class Solution {
 public:
-    void dfs(vector<vector<char>> &grid, int x, int y){
+    int dfs(vector<vector<int>> &grid, int x, int y){
         int rows = grid.size();
         int cols = grid[0].size();
-        if(x < 0 || y < 0 || x>= rows || y >= cols || grid[x][y] == '0'){
-            return;
+        if(x < 0 || y < 0 || x>= rows || y >= cols || grid[x][y] == 0){
+            return 0;
         }
-        grid[x][y] = '0';
-        dfs(grid,x-1,y);
-        dfs(grid,x+1,y);
-        dfs(grid,x,y-1);
+        grid[x][y] = 0;
+        return 1+
+        dfs(grid,x-1,y)+
+        dfs(grid,x+1,y)+
+        dfs(grid,x,y-1)+
         dfs(grid,x,y+1);
     }
-    int numIslands(vector<vector<char>>& grid) {
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
         int rows = grid.size(), cols = grid[0].size();
-        int count = 0;
+        int maxSum = 0;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if(grid[i][j]=='1'){
-                    count++;
-                    dfs(grid,i,j);
+                if(grid[i][j]==1){
+                    maxSum = max(dfs(grid,i,j),maxSum);
                 }
             } 
         }
-        return count;
-    }
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-
+        return maxSum;
     }
 };
 
