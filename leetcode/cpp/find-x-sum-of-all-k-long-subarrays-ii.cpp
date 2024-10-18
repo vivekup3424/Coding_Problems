4afdf448be5b727e330c7dabdca24529971682f2
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-10-19 03:56:53 +0530

    dsv sd

---
+++ leetcode/cpp/find-x-sum-of-all-k-long-subarrays-ii.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<long long> findXSum(vector<int>& nums, int k, int x) {
+        unordered_map<int, int> freq;
+        for(int i = 0; i < min(nums.size()))
+    }
+};
