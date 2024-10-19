bb5f046a56105a8c11ee7b8175f87274c897697b
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-10-19 08:31:41 +0530

    time a binary search tree

---
+++ leetcode/cpp/trim-a-binary-search-tree.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(root==nullptr){
+            return root;
+        }
+        else if(root->val < low){
+            root = trimBST(root->right,low,high);
+        }
+        else if(root->val > high){
+            root = trimBST(root->left,low,high);
+        }else{
+            root->left = trimBST(root->left,low,high);
+            root->right = trimBST(root->right,low,high);
+        }
+        return root;
+    }
+};
