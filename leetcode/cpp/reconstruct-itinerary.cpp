44b8d4110960e41ddf7f02aa1dae28ff568af19a
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-10-21 03:03:38 +0530

    prac

--- leetcode/cpp/reconstruct-itinerary.cpp
+++ leetcode/cpp/reconstruct-itinerary.cpp
@@ -18,34 +18,28 @@
 using namespace std;
 
 class Solution {
-private:
-    void dfs(unordered_map<string, multiset<string>> &m, string airport,vector<string> &result){
-        while(!m[airport].empty()){
-            auto it = *m[airport].begin();
-            m[airport].erase(it);
-            result.push_back(it);
-            dfs(m,it,result);
-        }
-    }
 public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        unordered_map<string, multiset<string>> graph;
-        int numTickets = tickets.size();
-        for(auto ticket : tickets){
-            graph[ticket[0]].insert(ticket[1]);
+        unordered_map<string, multiset<string>> m;
+        for (int i = 0; i < tickets.size(); i++) {
+            m[tickets[i][0]].insert(tickets[i][1]);
         }
+        
         vector<string> result;
-        result.push_back("JFK");
-        dfs(graph,"JFK",result);
+        dfs(m, "JFK", result);
+        reverse(result.begin(), result.end());
         return result;
     }
-};
-int main(){
-    vector<vector<string>> v = {{"MUC","LHR"},{"JFK","MUC"},{"SFO","SJC"},{"LHR","SFO"}};
-    auto t = Solution().findItinerary(v);
-    for(auto i : t){
-        cout<<i<<"=>";
+private:
+    void dfs(unordered_map<string, multiset<string>>& m,
+        string airport, vector<string>& result) {
+        
+        while (!m[airport].empty()) {
+            string next = *m[airport].begin();
+            m[airport].erase(m[airport].begin());
+            dfs(m, next, result);
+        }
+        
+        result.push_back(airport);
     }
-    cout<<"\n";
-    return 0;
-}
+};
