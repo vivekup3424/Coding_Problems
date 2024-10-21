6fa79ec26d9903140c403ba5e10f29cd143a906d
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-10-21 10:30:26 +0530

    sdlvksd

---
+++ oops/exceptionHandling.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int p,c,m,err = 0;
+  string name;
+  try {
+  cout<<"Enter physics marks:=";
+  cin>>p;
+  if(p<0 || p > 100){
+      err = throw(p);
+    }
+  } catch (err) {
+  
+  }
+}
