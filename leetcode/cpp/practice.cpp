5fcd7784fe34e35ec6a810e9ba4040108ee6335c
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-10-27 08:37:04 +0530

    git

--- leetcode/cpp/practice.cpp
+++ leetcode/cpp/practice.cpp
@@ -6,48 +6,24 @@ struct Comparator {
     }
 };
 
-class Solution {
-private:
-    string letters;
-    vector<bool> ans;
-
-    bool isPalindrome(const string& s) {
-        int l = 0, r = s.size() - 1;
-        while (l < r) {
-            if (s[l++] != s[r--]) {
-                return false;
+void selectionSort(vector<int> &v){
+    for(int i = 0; i < v.size(); i++){
+        int minIdx = i;
+        int minValue = v[i];
+        for(int j = i; j < v.size(); j++){
+            if(v[j] < minValue){
+                minIdx = j;
+                minValue = v[j];
             }
         }
-        return true;
+        swap(v[i],v[minIdx]);
     }
-
-public:
-    string dfs(map<int, set<int, Comparator>>& graph, string path, int source, int parent = -1) {
-        path.push_back(letters[source]);
-        for (auto child : graph[source]) {
-            if (child != parent) {
-                string s = dfs(graph, "", child, source);
-                ans[child] = isPalindrome(s); // Save the path for the child
-                path += s; // Append child's path after dfs
-            }
-        }
-        return path;
+}
+int main(){
+    vector<int> v = {15,90,45,67};
+    selectionSort(v);
+    for(auto it : v){
+        cout<<it<<" ";
     }
-
-    vector<bool> findAnswer(vector<int>& parent, const string& s) { 
-        letters = s;
-        int n = parent.size();
-        ans.resize(n);
-        map<int, set<int, Comparator>> graph;
-
-        for (int i = 0; i < n; i++) {
-            if (parent[i] != -1) {
-                graph[parent[i]].insert(i);
-            }
-        }
-
-        string path = dfs(graph, "", 0);
-        ans[0] = isPalindrome(path);
-        return ans;
-    }
-};
+    cout<<endl;
+}
---
+++ leetcode/cpp/single-number-2.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**Given an integer array nums where every element appears three times except for one, 
+ * which appears exactly once. Find the single element and return it.
+*/
+int singleNumber(vector<int> v){
+    
+}
