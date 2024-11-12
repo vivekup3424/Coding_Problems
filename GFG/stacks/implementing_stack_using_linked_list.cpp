37bcb7626e9f4faa9539b4a0760386aaa16a60ec
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-11-13 02:58:47 +0530

    stack implementation with linkedlist

--- GFG/stacks/implementing_stack_using_linked_list.cpp
+++ GFG/stacks/implementing_stack_using_linked_list.cpp
@@ -1,7 +1,46 @@
 //implementing stack using linked list
 
-template <typename t>
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+class Node{
+public:
+  int data;
+  Node *next;
+  Node(int _val):data(_val),next(nullptr){};
+};
 class Stack{
-private:
-
-}
+public:
+  Node *top;
+  Stack():top(nullptr){} //abusing initializer list
+  int pop(){
+    //removes element from the stack and returns it
+    //NOTE:- error = -1
+    try {
+    if(top==nullptr){
+        throw std::runtime_error("Stack Empty: Can't remove element it (⊙_☉)");
+      }
+      int data = top->data;
+      Node *nextNode = top->next;
+      delete top;
+      top = nextNode;
+      return data;
+    } catch (std::exception &e) {
+      std::cout<<e.what()<<std::endl;
+      return -1;
+    }
+  }
+  void push(int _val){
+    Node *newNode = new Node(_val);
+    newNode->next = top;
+    top = newNode;
+  }
+  ~Stack(){
+    while(top!=nullptr){
+      auto nextNode = top->next;
+      delete top;
+      top = nextNode;
+    }
+  }
+};
