2dfdecd024abbab95a826ca33155a3adde9e5787
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-11-16 01:19:54 +0530

    wes

--- websocket-demo/index.js
+++
@@ -1,5 +0,0 @@
-const http = require("http")
-const httpServer = http.createServer((_request, _response) => {
-	console.log("We have received a request");
-})
-httpServer.listen(8080, () => console.log("My server is listening on port 8080."))
---
+++ websocket-demo/websocket/README.md
@@ -0,0 +1,5 @@
+# Websocket from scrach with nodejs
+
+Here I am trying to implement the websocket project from scrach using nodejs
+and RFC 6455.
+
