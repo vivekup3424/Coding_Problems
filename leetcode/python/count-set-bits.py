60e44cc656b92f38f730f1ef3d56a905116bb03c
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-12-01 14:40:16 +0530

    sort 0 and 1

--- leetcode/python/count-set-bits.py
+++ leetcode/python/count-set-bits.py
@@ -1,9 +1,16 @@
+# def count_bits(x):
+#     num_bits = 0
+#     while x:
+#         num_bits += x & 1
+#         x >>= 1
+#     return num_bits
 def count_bits(x):
     num_bits = 0
     while x:
-        num_bits += x & 1
-        x >>= 1
-    return num_bits
+        num_bits += 1
+        x &= x - 1
+        print(f"num_bits : {num_bits}")
+        print(f"x : {x}")
 
 
 # n = int(input("Enter an integer:\t"))
