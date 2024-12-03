01aea38c7f0444adc63939e92cae320ad484dfc0
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-12-04 02:20:14 +0530

    add input

--- advent_of_code/2024/day3/1.py
+++ advent_of_code/2024/day3/1.py
@@ -1,2 +1,13 @@
-inputString = open("./input", mode="r").read()
-print(inputString)
+import re
+
+with open("./input", "r") as f:
+    data = f.read()
+
+print(
+    sum(
+        (
+            int(value_a) * int(value_b)
+            for value_a, value_b in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", data)
+        )
+    )
+)
