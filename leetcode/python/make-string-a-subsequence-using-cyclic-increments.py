21a5bc85db833945f5b8b6e85ba2d6fce66863cd
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-12-04 20:23:35 +0530

    daily question done

--- leetcode/python/make-string-a-subsequence-using-cyclic-increments.py
+++ leetcode/python/make-string-a-subsequence-using-cyclic-increments.py
@@ -1,16 +1,10 @@
 class Solution:
-    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
-        i, j = 0, 0
-        while i < len(str1) and j < len(str2):
-            if str1[i] == str2[j] or str1[(i + i) % 26] == str2[j]:
-                j += 1
-            i += 1
-        return j == len(str2)
-
-
-str1 = "abc"
-str2 = "ad"
-print(Solution().canMakeSubsequence(str1, str2))
-
-sdvnsd
-ldsknvs
+    def canMakeSubsequence(self, source: str, target: str) -> bool:
+        targetIdx, targetLen = 0, len(target)
+        for currChar in source:
+            if (
+                targetIdx < targetLen
+                and (ord(target[targetIdx]) - ord(currChar)) % 26 < 2
+            ):
+                targetIdx += 1
+        return targetIdx == targetLen
