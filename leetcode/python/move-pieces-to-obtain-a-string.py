ab0c52554ae48368b053bbdf1dc45e240ed61b3f
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-12-05 15:20:14 +0530

    some shit is going on

---
+++ leetcode/python/move-pieces-to-obtain-a-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+        # preprocessing on
+        return False
+
+
+A = Solution()
+A.canChange("_L__R__R_", "L______RR")
+A.canChange("R_L_", "__LR")
+A.canChange("_R", "R_")
