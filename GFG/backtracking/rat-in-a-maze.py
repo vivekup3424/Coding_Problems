f9c54cba5cda33d9c94f1660c19a54909308c879
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-12-06 06:15:14 +0530

    dfs changes

--- GFG/backtracking/rat-in-a-maze.py
+++ GFG/backtracking/rat-in-a-maze.py
@@ -1,3 +1,27 @@
-maze = [[1, 0, 0, 0], [1, 1, 0, 1], [0, 1, 0, 0], [1, 1, 1, 1]]
+maze = [[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]
 
 # rat can move through 1, but can't move through 0
+
+# moves are left,right,up,down
+moves = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+total_moves = []
+
+
+def dfs(x, y, path, maze):
+    rows = len(maze)
+    cols = len(maze[0])
+    if x == rows - 1 and y == cols - 1:
+        total_moves.append(path)
+        return
+    if x < 0 or x >= rows or y < 0 or y >= cols or maze[x][y] == 0:
+        return
+    maze[x][y] = 0
+    for dx, dy in moves:
+        newx = x + dx
+        newy = y + dy
+        dfs(newx, newy, path + [(newx, newy)], maze)
+
+
+dfs(0, 0, [(0, 0)], maze)
+for path in total_moves:
+    print(path)
