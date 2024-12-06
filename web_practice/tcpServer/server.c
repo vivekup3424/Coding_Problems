1c2a7b9903e9cf69aecd9952f479f41e3a5ab0e3
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-12-07 00:20:26 +0530

    add some tcp code

---
+++ web_practice/tcpServer/server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+int main(int argc, char *argv[])
+{
+	printf("Hello World\n");
+	return 0;
+}
