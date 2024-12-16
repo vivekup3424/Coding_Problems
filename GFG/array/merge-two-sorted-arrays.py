bd06387e0cae8fec5d6d6c476e19b4931ab0f36f
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-12-16 23:43:22 +0530

    added unit test in DSA

--- GFG/array/merge-two-sorted-arrays.py
+++ GFG/array/merge-two-sorted-arrays.py
@@ -2,42 +2,82 @@
 # instead of the beginning. This avoids the need to shift elements multiple times.
 
 from typing import List
+import unittest
 
 
-class Solution:
-    def mergeArrays(self, a: List[int], b: List[int]):
-        i, j = len(a) - 1, len(b) - 1
-        for _ in range(len(b)):
-            a.append(0)
-        k = len(a) - 1
-        while i >= 0 and j >= 0:
-            if a[i] >= b[j]:
-                a[k] = a[i]
-                k -= 1
-                i -= 1
-            else:
-                a[k] = b[j]
-                k -= 1
-                j -= 1
-        while i >= 0:
+def mergeArrays(a: List[int], b: List[int]):
+    i, j = len(a) - 1, len(b) - 1
+    for _ in range(len(b)):
+        a.append(0)
+    k = len(a) - 1
+    while i >= 0 and j >= 0:
+        if a[i] >= b[j]:
             a[k] = a[i]
             k -= 1
             i -= 1
-        while j >= 0:
+        else:
             a[k] = b[j]
             k -= 1
             j -= 1
-        return a
+    while i >= 0:
+        a[k] = a[i]
+        k -= 1
+        i -= 1
+    while j >= 0:
+        a[k] = b[j]
+        k -= 1
+        j -= 1
 
 
-def main():
-    # n = int(input())
-    a = [2, 4, 7, 10]
-    # m = int(input())
-    b = [2, 3]
-    ob = Solution()
-    print(ob.mergeArrays(a, b))
+class TestMergeInPlace(unittest.TestCase):
+    def test_typical_case(self):
+        A = [1, 3, 5]
+        B = [2, 4, 6]
+        mergeArrays(A, B)
+        self.assertEqual(A, [1, 2, 3, 4, 5, 6])
+
+    def test_empty_B(self):
+        A = [1, 2, 3]
+        B = []
+        mergeArrays(A, B)
+        self.assertEqual(A, [1, 2, 3])
+
+    def test_empty_A(self):
+        A = []
+        B = [1, 2, 3]
+        mergeArrays(A, B)
+        self.assertEqual(A, [1, 2, 3])
+
+    def test_both_empty(self):
+        A = []
+        B = []
+        mergeArrays(A, B)
+        self.assertEqual(A, [])
+
+    def test_overlapping_values(self):
+        A = [1, 2, 3]
+        B = [2, 3, 4]
+        mergeArrays(A, B)
+        self.assertEqual(A, [1, 2, 2, 3, 3, 4])
+
+    def test_single_element_in_A(self):
+        A = [1]
+        B = [2]
+        mergeArrays(A, B)
+        self.assertEqual(A, [1, 2])
+
+    def test_single_element_in_B(self):
+        A = []
+        B = [1]
+        mergeArrays(A, B)
+        self.assertEqual(A, [1])
+
+    def test_large_arrays(self):
+        A = [1, 3, 5, 7, 9]
+        B = [2, 4, 6, 8, 10]
+        mergeArrays(A, B)
+        self.assertEqual(A, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
