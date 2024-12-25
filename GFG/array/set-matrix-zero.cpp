37652140d15b6ba4746fd4cc2cbc266e219fee43
vivekKEUS <vivek.upadhyay@keus.in>
2024-12-25 11:28:13 +0000

    commiting the error into the code

--- GFG/array/set-matrix-zero.cpp
+++ GFG/array/set-matrix-zero.cpp
@@ -1,6 +1,5 @@
-//give an m x n integer matrix, if an element is 0,
-//then set its entire row and column to 0
+// give an m x n integer matrix, if an element is 0,
+// then set its entire row and column to 0
 
 #include <bits/stdc++.h>
 using namespace std;
-
