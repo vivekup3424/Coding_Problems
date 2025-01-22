b65678daa9be57f460d504d3af15be0997644fc7
vivekKEUS <vivek.upadhyay@keus.in>
2025-01-22 22:47:56 +0530

    grid game

---
+++ leetcode/cpp/grid-game.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        int rows = grid.size(), cols = grid[0].size();
+        for(int i = 0; i < rows; i++){
+            
+        }
+    }
+};
---
+++ nodejs-practice/1.mjs
@@ -0,0 +1,2 @@
+import EventEmitter from "events"
+const eventEmitter = new EventEmitter()
