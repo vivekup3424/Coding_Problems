62313391aa1906e4447f0798fa45d917278075f9
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-02-28 16:32:41 +0530

    Added a small math service for practice purposes

---
+++ moleculerPractice/channelDemo/math.service.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "math",
+    actions: {
+        add(ctx) {
+            return Number(ctx.params.a) + Number(ctx.params.b);
+        },
+
+        sub(ctx) {
+            return Number(ctx.params.a) - Number(ctx.params.b);
+        }
+    }
+}
