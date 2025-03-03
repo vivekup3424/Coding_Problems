9f48fc8e8eb3e6c84863755e62651e07e02c9eca
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-03-03 10:16:29 +0530

    removed the java code of lovepreet singh

--- lovepreet_singh/irctc/app/src/main/java/ticket/booking/services/TrainService.java
+++
@@ -1,5 +0,0 @@
-package ticket.booking.services;
-
-public class TrainService {
-    
-}
--- lovepreet_singh/irctc/app/src/main/java/ticket/booking/services/UserBookingService.java
+++
@@ -1,9 +0,0 @@
-package ticket.booking.services;
-import ticket.booking.entities.*;
-
-public class UserBookingService {
-    private User user;
-    public UserBookingService(User user){
-        this.user = user;
-    }
-}
