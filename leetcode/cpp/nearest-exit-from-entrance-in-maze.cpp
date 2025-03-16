7f32b6a33b34ba6552693ff5568ba24e4520802a
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-03-16 23:03:47 +0530

    copy and pasta

---
+++ leetcode/cpp/nearest-exit-from-entrance-in-maze.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        int rows = maze.size(), cols = maze[0].size();
+        vector<vector<bool>> visited(rows,vector<bool>(cols,false));
+        queue<pair<int,int>> 
+    }
+};
