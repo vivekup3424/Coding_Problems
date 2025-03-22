7356ac9f312b9f3fa052bc0bba5bb08383a8b95c
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-03-23 00:21:58 +0530

    string to interger python

---
+++ leetcode/python/binary-tree-inorder-traversal.py
@@ -0,0 +1,8 @@
+from typing import Optional, List
+class TreeNode:
+    def __init__(self,val = 0, left = None, right = None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+
