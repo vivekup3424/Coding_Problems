0a06183b5c359a81d4f5f59fd7d26ee847112c11
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-03-23 23:09:10 +0530

    properties graph

---
+++ leetcode/cpp/properties-graph.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int intersect(vector<int> &a, vector<int> &b){
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        int common_elements = 0;
+    }
+    int numberOfComponents(vector<vector<int>> &properties, int k){
+    }
+};
