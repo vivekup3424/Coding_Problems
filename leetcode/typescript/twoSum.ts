c454ef22b0f5d5b54063873666eb9a0cb8afc8b3
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-04-03 21:56:27 +0530

    two sum code add

---
+++ leetcode/typescript/twoSum.ts
@@ -0,0 +1,7 @@
+function twoSum(nums: number[], target: number):number[]{
+    const hashMap = new Map<number,number>();
+    for(let i = 0; i < nums.length;i++){
+        
+    }
+    return [-1,-1]
+}
