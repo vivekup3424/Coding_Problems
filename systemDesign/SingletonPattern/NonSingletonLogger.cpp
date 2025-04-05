b83d3b9eef8116e5d9fe3825ed50778873454577
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-04-05 14:52:29 +0530

    sdksmkfdmsd

---
+++ systemDesign/SingletonPattern/NonSingletonLogger.cpp
@@ -0,0 +1,16 @@
+class NonSingletonLogger
+{
+private:
+    readonly 
+public:
+    NonSingletonLogger(/* args */);
+    ~NonSingletonLogger();
+};
+
+NonSingletonLogger::NonSingletonLogger(/* args */)
+{
+}
+
+NonSingletonLogger::~NonSingletonLogger()
+{
+}
