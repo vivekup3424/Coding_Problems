b484f4175e1259178d8aacaac24f96ffbb4aaba1
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-04-09 19:20:16 +0530

    sdlkvmklsdv

---
+++ GFG/stacks/stack-using-two-queues.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class QueueStack{
+private:
+    queue<int> q1;
+    queue<int> q2;
+public:
+    void push(int);
+    int pop();
+};
+void QueueStack ::push(int x) {
+    // code here
+    
+}
+
+int QueueStack ::pop() {
+    
+    // code here
+}
