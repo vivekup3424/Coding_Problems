9ae160e9c20efa60d6878e2e85848aad5235164c
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-04-10 18:23:34 +0530

    ;ldsmvsd

---
+++ vimPractice/main.rs
@@ -0,0 +1,7 @@
+function foo(blaz : {oddly: "long" | "type", but: "hey" | "this" | "is" | number}){
+    //do something here
+}
+
+function new_fn(bar: {oddly: "long" | "type", but: "hey" | "this" | "is" | number}){
+    // ...
+}
