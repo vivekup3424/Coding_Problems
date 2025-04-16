4006189cd5cb65d8fab06643a8760146b594304d
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-04-16 17:40:45 +0530

    clusy factorial python:

--- leetcode/cpp/clumsy-factorial.cpp
+++ leetcode/cpp/clumsy-factorial.cpp
@@ -1,5 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution{
-
-}
+public:
+    int clumsy(int n){
+        if(n<=2){
+            return n;
+        }
+        else if(n==3){
+            return 3 * clumsy(2);
+        }else{
+            return (n+1)-clumsy(n);
+        }
+    }
+};
---
+++ leetcode/python/clumsy-factorial.py
@@ -0,0 +1,12 @@
+class Solution:
+    def clumsy(self,N:int)->int:
+        if N<=2:
+            return N
+        elif N <= 4:
+            return N+3
+        elif (N-4)%4==0:
+            return N+1
+        elif (N-4)%4<=2:
+            return N+2
+        else:
+            return N-1
