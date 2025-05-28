65178562509b97a5f8d85f30c0eeb1ea38f1f3ef
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-05-29 00:07:07 +0530

    apply merge sort in java

---
+++ java_practice/first/ArrayList.java
@@ -0,0 +1,7 @@
+import java.util.List;
+import java.util.RandomAccess;
+import java.util.AbstractList;
+
+public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAccess, Cloneable, java.io.Serializable{
+
+}
