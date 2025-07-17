8c4a0e2aa0e001f34b96c6d56fa4b1af029f5fe5
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-07-17 09:22:03 +0530

    Add ListNode class and initial implementation for asteroid collision solution

---
+++ leetcode/java/ListNode.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    };
+
+    ListNode(int _val) {
+        this.val = _val;
+    }
+
+    ListNode(int _val, ListNode _next){this.val = _val; this}
+}
---
+++ leetcode/java/asteroid-collision.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Solution{
+    public int[] asteroidCollision(int[] asteroids){
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i =0; i < asteroids.length; i++);
+    }
+}
