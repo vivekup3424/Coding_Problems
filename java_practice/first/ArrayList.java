4a9332930ced8cdd3ae9c9b93499e3d38903e3a0
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-07-22 23:10:30 +0530

    removing bloat

--- java_practice/first/ArrayList.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-import java.util.RandomAccess;
-import java.util.AbstractList;
-
-public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAccess, Cloneable, java.io.Serializable{
-
-}
