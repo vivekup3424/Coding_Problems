b62cc8f237b3f3e78fd73ef9bf24b7ac430abcd5
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-08-23 17:14:13 +0530

    Refactor nextPermutation method to simplify logic and remove unused early detection

--- leetcode/java/NextPermutation.java
+++ leetcode/java/NextPermutation.java
@@ -1,38 +1,26 @@
-import java.util.*;
-
 public class NextPermutation {
-    private boolean isIncreasing(int[] nums){
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i]<nums[i-1]){
-                return false;
-            }
-        }
-        return true;
-    }
     public void nextPermutation(int[] nums) {
-        //early detection
-        if(isIncreasing(nums)){
-            int i = 0, j = nums.length-1;
-            while(i<j){
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+        int pivot_idx = -1;
+        for(int i = nums.length-1; i > 0; i++){
+            if(nums[i-1]<nums[i]){
+                pivot_idx = i;
+                break;
             }
-            return;
+        } 
+        //reverse the array 
+        int l = pivot_idx, r = nums.length-1;
+        while(l<r){
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
         }
-        for (int i = nums.length - 1; i >= 0; i--) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (nums[j] < nums[i]) {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                    System.out.printf(" j = %d, i = %d\n", i, j);
-                    if (j + 1 < i - 1) {
-                        Arrays.sort(nums, j + 1, i);
-                    }
-                    return;
-                }
+        if(pivot_idx!=-1){
+            for(int i = pivot_idx; i < nums.length; i++){
+                if(nums[i]>nums[pivot_idx-1])
             }
         }
+        return ;
     }
 }
