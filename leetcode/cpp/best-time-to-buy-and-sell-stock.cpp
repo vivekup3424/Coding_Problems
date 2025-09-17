9d03a88bcf1f9004bcbe3b956393dd5e0605ff31
vivekup3424 <vivek.hyfrt3424@gmail.com>
2025-09-18 02:05:08 +0530

    ggg

---
+++ leetcode/cpp/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int maxProfit(vector<int> &prices){
+        int currentProfit = 0, maxProfit = 0;
+        for(int i = 1; i < prices.size();i++){
+            currentProfit+=(prices[i]-prices[i-1]);
+            if(currentProfit<0){
+                currentProfit = 0;
+            }
+            maxProfit = max(maxProfit,currentProfit);
+        }
+        return currentProfit;
+    }
+};
