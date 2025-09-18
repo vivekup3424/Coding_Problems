deacd28cca8863591d8f98fd66b2f97b177c3ac5
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-09-19 00:55:00 +0530

    Add implementation for minimum number of jumps in an array

---
+++ GFG/array/minimum-number-of-jumps.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int minJumps(vector<int> & arr){
+        int n = arr.size();
+        vector<int> jumps(n, INT_MAX);
+        jumps[0]=0;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j <= i+arr[i] && j < n; j++){
+                jumps[j] = min(jumps[j], jumps[i]+1);
+            }
+        }
+        return jumps[n-1];
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    vector<int> arr = {1,3,5,8,9,2,6,7,6,8,9};
+    Solution *A = new Solution();
+    int answer = A->minJumps(arr);
+    cout<<answer<<endl;
+    return 0;
+}
