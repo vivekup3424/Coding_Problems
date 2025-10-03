d135e07a38cacf13a6a6c98ef2dd293d1b9719f6
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-10-03 22:04:36 +0530

    Refactor Node structure and improve priority queue implementation in trapping rain water solution

---
+++ leetcode/cpp/lambda.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    auto subtract = [](int a, int b){ return a - b;};
+}
--- leetcode/cpp/trapping-rain-water-ii.cpp
+++ leetcode/cpp/trapping-rain-water-ii.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
+typedef struct node{
     int x, y, h;
-    Node(int _x, int _y, int _h) : x(_x), y(_y), h(_h) {}
-};
+    node(int _x, int _y, int _h) : x(_x), y(_y), h(_h) {};
+    bool operator<(const node &b) const {
+        return h > b.h;
+    }
+}Node;
 
 class Solution {
     public:
@@ -13,8 +16,41 @@ class Solution {
         int cols = heightMap[0].size();
         if(rows < 3 || cols < 3) return 0;
         vector<vector<bool>> visited(rows, vector<bool>(cols, false));
-        priority_queue<Node, vector<Node>, [](Node &a, Node &b){
-            return a.h > b.h;
-        }> minHeap;
+        priority_queue<Node, vector<Node>, greater<Node>> minHeap;
+        int totalWater = 0;
+        for(int i = 0; i < rows; i++){
+            minHeap.push(Node(i, 0, heightMap[i][0]));
+            minHeap.push(Node(i, cols - 1, heightMap[i][cols - 1]));
+            visited[i][0] = true;
+            visited[i][cols - 1] = true;
+        }
+        for(int j = 1; j < cols - 1; j++){
+            minHeap.push(Node(0, j, heightMap[0][j]));
+            minHeap.push(Node(rows - 1, j, heightMap[rows - 1][j]));
+            visited[0][j] = true;
+            visited[rows - 1][j] = true;
+        }
+        vector<int> dirX = {0, 0, 1, -1};
+        vector<int> dirY = {1, -1, 0, 0};
+        while(!minHeap.empty()){
+            auto node = minHeap.top();
+            minHeap.pop();
+            for(int i = 0; i < 4; i++){
+                auto neighborX = node.x + dirX[i];
+                auto neighboxY = node.y + dirY[i];
+                if(0 < neighborX && neighborX < rows &&
+                    0 < neighboxY && neighboxY < cols && 
+                    !visited[neighborX][neighboxY]){
+                        auto neighborHeight = heightMap[neighborX][neighboxY];
+                        if(neighborHeight < node.h){
+                            totalWater += (node.h - neighborHeight);
+                        }
+                        auto newHeight = max(node.h, neighborHeight);
+                        visited[neighborX][neighboxY] = true;
+                        minHeap.push({neighborX,neighboxY,neighborHeight});
+                    }
+            }
+        }
+        return totalWater;
     }
-}
+};
