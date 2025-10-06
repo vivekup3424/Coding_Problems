4bd0c233b0c46eeeff35fc0c399356d608d309be
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-10-06 23:51:39 +0530

    Add solution for Codeforces problem 1837A: Implement main logic and input handling

---
+++ codeforces/1837A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,k;
+        cin>>x>>k;
+        if(x%k!=0){
+            cout<<1<<endl;
+            cout<<x<<endl;
+        }else{
+            cout<<2<<endl;
+            cout<<1<<" "<<x-1<<endl;
+        }
+    }
+}
