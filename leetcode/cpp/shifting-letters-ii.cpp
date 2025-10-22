f929fbaa512b1146b7f1b6b0f97b5acc59d2146f
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-10-22 22:00:01 +0530

    Auto-commit: 2025-10-22 22:00:01

--- leetcode/cpp/shifting-letters-ii.cpp
+++ leetcode/cpp/shifting-letters-ii.cpp
@@ -1,28 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
-    public:
-    string shiftingLetters(string s, vector<vector<int>> shifts){
+class Solution
+{
+public:
+    string shiftingLetters(string s, vector<vector<int>> shifts)
+    {
         int n = s.size();
-        vector<int> totalShift(n,0);
-        for(int i = 0; i < shifts.size(); i++){
+        vector<int> totalShift(n, 0);
+        for (int i = 0; i < shifts.size(); i++)
+        {
             int firstIdx = shifts[i][0];
             int lastIdx = shifts[i][1];
-            int direction = (shifts[i][1] == 1 ? 1 : -1);
-            totalShift[firstIdx]+=direction;
-            if(lastIdx+1<n){
-                totalShift[lastIdx+1]-=direction;
+            int direction = (shifts[i][2] == 1 ? 1 : -1);
+            totalShift[firstIdx] += direction;
+            if (lastIdx + 1 < n)
+            {
+                totalShift[lastIdx + 1] -= direction;
             }
         }
-        for(int i = 0;i < n; i++){
-            if(i==0){
-                totalShift[i] = totalShift[i];
-            }
+        for (int i = 1; i < n; i++)
+        {
+            totalShift[i] += totalShift[i - 1];
         }
-        for(int j = 0; j < n; j++){
+        for (int j = 0; j < n; j++)
+        {
             int val = s[j] - 'a';
-
+            int shiftedVal = (totalShift[j] + val + 26) % 26; // normalize the neg vals
+            s[j] = 'a' + shiftedVal;
         }
+        return s;
     }
-}
+};
