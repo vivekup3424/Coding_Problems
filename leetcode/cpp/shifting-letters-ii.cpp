62655915dc11d355b626bb2c281fe52b9a3baf5d
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-10-23 00:30:01 +0530

    Auto-commit: 2025-10-23 00:30:01

--- leetcode/cpp/shifting-letters-ii.cpp
+++ leetcode/cpp/shifting-letters-ii.cpp
@@ -7,17 +7,14 @@ public:
     string shiftingLetters(string s, vector<vector<int>> shifts)
     {
         int n = s.size();
-        vector<int> totalShift(n, 0);
+        vector<int> totalShift(n+1, 0);
         for (int i = 0; i < shifts.size(); i++)
         {
             int firstIdx = shifts[i][0];
             int lastIdx = shifts[i][1];
             int direction = (shifts[i][2] == 1 ? 1 : -1);
             totalShift[firstIdx] += direction;
-            if (lastIdx + 1 < n)
-            {
-                totalShift[lastIdx + 1] -= direction;
-            }
+            totalShift[lastIdx + 1] -= direction;
         }
         for (int i = 1; i < n; i++)
         {
@@ -26,8 +23,8 @@ public:
         for (int j = 0; j < n; j++)
         {
             int val = s[j] - 'a';
-            int shiftedVal = (totalShift[j] + val + 26) % 26; // normalize the neg vals
-            s[j] = 'a' + shiftedVal;
+            totalShift[j] = (totalShift[j] + 26) % 26; // normalize the neg vals
+            s[j] = s[j]+totalShift[j];
         }
         return s;
     }
