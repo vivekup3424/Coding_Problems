2ade359304c40f0112f699192d6fb5d407aaf8c9
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-10-23 23:00:01 +0530

    Auto-commit: 2025-10-23 23:00:01

---
+++ leetcode/3461.check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=3461 lang=cpp
+ *
+ * [3461] Check If Digits Are Equal in String After Operations I
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string sumDigits(string s){
+        
+    }
+    bool hasSameDigits(string s) {
+    }
+};
+// @lc code=end
+
