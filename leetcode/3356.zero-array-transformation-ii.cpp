ca0c87fa10deaedba117e0a11997f4dddf1f51f0
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-10-23 23:40:01 +0530

    Auto-commit: 2025-10-23 23:40:01

--- leetcode/3356.zero-array-transformation-ii.cpp
+++ leetcode/3356.zero-array-transformation-ii.cpp
@@ -7,13 +7,31 @@
 // @lc code=start
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 private:
     bool isItPossible(vector<int> &nums, vector<vector<int>> &queries)
+    {
+        int n = nums.size();
+        vector<int> freq(n + 1, 0);
+        for (auto query : queries)
+        {
+            auto l = query[0], r = query[1], val = query[2];
+            freq[l] = val;
+            freq[r + 1] = -val;
+        }
+        for(int i = 1; i < n; i++){
+            freq[i]+=freq[i-1];
+        }
+        for(int i = 0; i < n; i++){
+            if(nums[i]>freq[i]) return false;
+        }
+        return true;
+    }
+
 public:
-    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        
+    int minZeroArray(vector<int> &nums, vector<vector<int>> &queries)
+    {
     }
 };
 // @lc code=end
-
