2c5c440b9b18330f9b174d9394d8a3776e60c936
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-10-23 23:20:01 +0530

    Auto-commit: 2025-10-23 23:20:01

--- leetcode/3355.zero-array-transformation-i.cpp
+++ leetcode/3355.zero-array-transformation-i.cpp
@@ -7,17 +7,25 @@
 using namespace std;
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-       int n = nums.size();
-       vector<int> freq(n+1,0) ;
-       for(auto q : queries){
-        auto l = q[0], r = q[1];
-        freq[l] = 1;
-        freq[r+1] = -1;
-       }
+    bool isZeroArray(vector<int> &nums, vector<vector<int>> &queries)
+    {
+        int n = nums.size();
+        vector<int> freq(n + 1, 0);
+        for (auto q : queries)
+        {
+            auto l = q[0], r = q[1];
+            freq[l] = 1;
+            freq[r + 1] = -1;
+        }
+        for(int i = 1; i < n; i++){
+            if(nums[i] > freq[i]){
+                return false;
+            }
+        }
+        return true;
     }
 };
 // @lc code=end
-
---
+++ leetcode/3356.zero-array-transformation-ii.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=3356 lang=cpp
+ *
+ * [3356] Zero Array Transformation II
+ */
+
+// @lc code=start
+class Solution {
+private:
+    bool isItPossible(vector<int> &nums, vector<vector<int>> &queries)
+public:
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        
+    }
+};
+// @lc code=end
+
