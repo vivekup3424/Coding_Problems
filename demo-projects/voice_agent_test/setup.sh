aca58443bd5c67d2a47d4bce36cc3d9ce429dab1
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-10-27 11:10:01 +0530

    Auto-commit: 2025-10-27 11:10:01

--- demo-projects/voice_agent_test/setup.sh
+++ demo-projects/voice_agent_test/setup.sh
@@ -1,2 +1,12 @@
+pyenv_path="$HOME/.pyenv"
+if [ -d "$pyenv_path" ]; then
+    echo "pyenv is already installed at $pyenv_path"
+    exit 0
+fi
+
+# setting up shell environment for pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
 
 curl -fsSL https://pyenv.run | bash
