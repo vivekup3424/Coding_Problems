6aae9ea5c82ffca1db2874e487fc4ccb4cc25591
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-10-27 16:40:01 +0530

    Auto-commit: 2025-10-27 16:40:01

--- demo-projects/livekit-sdk-practice/create-room.py
+++ demo-projects/livekit-sdk-practice/create-room.py
@@ -5,5 +5,8 @@ async def main():
     lkapi = api.LiveKitAPI("http://localhost:7880", "devKey", "secret")
     room_info = await lkapi.room.create_room(api.CreateRoomRequest(name="my-room"))
     print("room_info:", room_info)
+    results = await lkapi.room.list_rooms(api.ListRoomsRequest)
+    print("results:", results)
+    await lkapi.aclose()
 
 asyncio.run(main())
