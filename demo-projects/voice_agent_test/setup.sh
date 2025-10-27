3fd55b49e5018e69af4a8ab4ea0a1fa48d5df7b5
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-10-27 11:20:01 +0530

    Auto-commit: 2025-10-27 11:20:01

--- demo-projects/voice_agent_test/setup.sh
+++ demo-projects/voice_agent_test/setup.sh
@@ -1,12 +1,27 @@
 pyenv_path="$HOME/.pyenv"
 if [ -d "$pyenv_path" ]; then
-    echo "pyenv is already installed at $pyenv_path"
-    exit 0
+    rm -rf $pyenv_path # remove this code in final version
+    # echo "pyenv is already installed at $pyenv_path"
+    # exit 0
 fi
 
+SHELL_NAME=$(basename "$SHELL")
+if [ "$SHELL_NAME" == "zsh"]; then
+    SHELL_CONFIG="$HOME/.zshrc"
+elif [ "$SHELL_NAME" == "bash" ]; then
+    SHELL_CONFIG="$HOME/.bashrc"
+else
+    echo "Unsupported shell: $SHELL_NAME"
+    exit 1
+fi
 # setting up shell environment for pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $SHELL_CONFIG
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> $SHELL_CONFIG
+echo 'eval "$(pyenv init - bash)"' >> $SHELL_CONFIG
 
 curl -fsSL https://pyenv.run | bash
+
+source $SHELL_CONFIG
+
+value=$(pyenv help)
+echo $value
