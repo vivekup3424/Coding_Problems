5394863841c8d3dd4627d31f433fd5e7cddfd596
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-12-23 23:07:42 +0530

    add some dsa code

---
+++ leetcode/javascript/two-best-non-overlapping-events.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} events
+ * @return {number}
+ */
+var maxTwoEvents = function(events){
+    events.sort((a,b)=> a[1]-b[1]);
+    const n = events.length;
+    let start = 0, end = 0;
+    let visited = new Array(n).fill(false);
+    for(let i = 0; i < n; i++){
+        if()
+    }
+}
