9d191fbc222c356c1fdcd54ea1150e9f8302c1dc
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-12-24 15:40:25 +0530

    Add map function implementation for array transformation in JavaScript

---
+++ leetcode/javascript/apply-transformation-over-each-element-in-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+    return arr.map(fn);
+}
+
+var map = function (arr, fn) {
+    const result = []
+    for(let i = 0; i < arr.length; i++){
+       result.push(fn(arr[i], i)) 
+    }
+    return result;
+}
