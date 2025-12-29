a22fbb90b508c8d46871e27d6b889161c87d711f
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-12-29 15:18:14 +0530

    add two more files

---
+++ leetcode/javascript/array-wrapper.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.nums = Array.isArray(nums) ? nums : [];
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    let sum = 0;
+    for(const num of this.nums){
+        sum += num
+    }
+    return sum
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return `[${this.nums.join(',')}]`;
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
