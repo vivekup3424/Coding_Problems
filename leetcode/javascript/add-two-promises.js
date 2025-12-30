a3c3b2a13829119275b4daa71c3276c64bbfe505
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-12-30 13:23:31 +0530

    add two promises

--- leetcode/javascript/add-two-promises.js
+++ leetcode/javascript/add-two-promises.js
@@ -3,4 +3,7 @@
  * @param {Promise} promise2
  * @return {Promise}
  */
-var addT
+var addTwoPromises = async function(promise1, promise2){
+    const values = await Promise.all([promise1,promise2]);
+    return values[0]+values[1];
+};
