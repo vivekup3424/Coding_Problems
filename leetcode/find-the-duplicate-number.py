ac62e8812574a0ba885493f43b5f13e5ffbd0d35
vivekup3424 <vivek.hygfrt3424@gmail.com>
2025-12-30 15:25:31 +0530

    feat: Remove deprecated solutions and add sortBy function in JavaScript

--- leetcode/find-the-duplicate-number.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        s = set()
-        for n in nums:
-            if n in s:
-                return n
-            s.add(n)
-        return -1
