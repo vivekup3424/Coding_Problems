9d62aec97176c856c9fc67f7bb2b1e3f36db3d2c
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-01-02 17:31:18 +0530

    feat: implement TimeLimitedCache class with set, get, and count methods

---
+++ leetcode/javascript/cache-with-time-limit.js
@@ -0,0 +1,39 @@
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    let found = this.cache.has(key);
+    this.cache.set(key,{
+        value,
+        ref
+    })
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
