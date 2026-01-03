952f805c02a4cf9c7849bc2c8651342d6acc6ee4
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-01-03 17:39:46 +0530

    feat: simplify check for sorted and rotated array; remove unnecessary methods feat: add removeDuplicates function to eliminate duplicates from sorted array

--- leetcode/python/check-if-array-is-sorted-and-rotated.py
+++ leetcode/python/check-if-array-is-sorted-and-rotated.py
@@ -1,22 +1,10 @@
 from typing import List
 class Solution:
-    def isSorted(self,left: int, right: int, nums: List[int]) -> bool:
-        for i in range(left+1,right):
-            if(nums[i]<nums[i-1]):
-                return False
-        return True
-    def check(self,nums: List[int]) -> bool:
-        peak_index = -1
+    def check(self, nums: List[int]) -> bool:
+        num_pivot = 0
         for i in range(len(nums)):
-            isLeftSidePeaking = True
-            if(i-1>=0 and nums[i] < nums[i-1]):
-                isLeftSidePeaking = False
-            isRightSidePeaking = True
-            if(i+1<len(nums) and nums[i] < nums[i+1]):
-                isRightSidePeaking = False
-            if(isLeftSidePeaking and isRightSidePeaking):
-                peak_index = i
-
-            if(self.isSorted(0,peak_index,nums) and self.isSorted(peak_index,len(nums),nums)):
-                return True
-        return False
+            if(i+1<len(nums) and nums[i] > nums[i+1]):
+                num_pivot+=1
+        if nums[-1] > nums[0]:
+            num_pivot+=1
+        return num_pivot <= 1
