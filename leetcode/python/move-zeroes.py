be5abfaf92e425c3ced0c1361952a346d8c81201
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-01-03 18:38:02 +0530

    feat: implement moveZeroes function to shift zeros in an array

---
+++ leetcode/python/move-zeroes.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j+=1
