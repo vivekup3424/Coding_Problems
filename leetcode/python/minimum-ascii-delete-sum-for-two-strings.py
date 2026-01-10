7d666f2f617b73d5dd1ab13980fd78c4d51e3535
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-01-10 17:09:23 +0530

    add some dp question's solution

---
+++ leetcode/python/minimum-ascii-delete-sum-for-two-strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalASCIISum(self,s1: str)->int:
+        answer = 0
+        for c in s1:
+            answer += ord(c)
+        return answer
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        n = len(s1)
+        m = len(s2)
+        dp = [[0] * (m+1) for _ in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if(s1[i-1]==s2[j-1]):
+                    dp[i][j] = ord(s1[i-1]) + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        return self.totalASCIISum(s1) + self.totalASCIISum(s2) - 2 * dp[n][m]
