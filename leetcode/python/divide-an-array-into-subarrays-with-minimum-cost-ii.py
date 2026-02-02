7af583430554bace1eb0be87b08d3a792473720f
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-02-02 18:21:17 +0530

    add some mroe code

---
+++ leetcode/python/divide-an-array-into-subarrays-with-minimum-cost-ii.py
@@ -0,0 +1,4 @@
+from typing import List
+class Solution:
+    def minimumCost(self, nums: List[int], k: int, dist: int) -> int:
+        # we will have to 
---
+++ leetcode/python/search-in-rotated-sorted-array.py
@@ -0,0 +1,4 @@
+from typing import List
+class Solution:
+    def searchElement(self, numbers: List[int], value: int):
+        
