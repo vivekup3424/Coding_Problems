b7bfd2bc2aa671123f70f2f3e121521b1c1ee892
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-02-03 22:21:19 +0530

    add the code for online-stock-span problem

--- leetcode/python/online-stock-span.py
+++ leetcode/python/online-stock-span.py
@@ -1,4 +1,14 @@
+from typing import List
 class StockSpanner:
     def __init__(self):
-        self.stack = [] #stores the indices of elements
+        self.stack: List[tuple[int,int]]= [] #stores the indices of elements
+        self.index = 0
     
+    def next(self, price: int) -> int:
+        while len(self.stack) > 0 and self.stack[-1][1] <= price:
+            self.stack.pop()
+
+        value = self.index + 1 if len(self.stack) == 0 else self.index - self.stack[-1][0]
+        self.stack.append((self.index,price))
+        self.index+=1
+        return value
