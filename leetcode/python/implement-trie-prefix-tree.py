9d9ad34e8219b20e508a01a42ca27492e419dcbc
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-02-04 21:36:27 +0530

    add code for ip_finder

---
+++ leetcode/python/implement-trie-prefix-tree.py
@@ -0,0 +1,43 @@
+from typing import Optional
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
+
+    def search(self, word: str) -> bool:
+        node = self._find_node(word)
+        return node is not None and node.is_end
+
+    def startsWith(self, prefix: str) -> bool:
+        return self._find_node(prefix) is not None
+
+    def _find_node(self, prefix: str) -> Optional["TrieNode"]:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return None
+            node = node.children[char]
+        return node
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
