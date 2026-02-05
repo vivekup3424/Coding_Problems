6ba80f292db4b0618a93a6b2e5dd1c70566372da
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-02-05 12:50:23 +0530

    some more DSA code

--- leetcode/python/letter-combinations-of-a-phone-number.py
+++ leetcode/python/letter-combinations-of-a-phone-number.py
@@ -1,6 +1,20 @@
+from typing import List
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         characters_map = {
             "2": ["a", "b", "c"],
-            "3": []
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
         }
+        answer : List[str] = []
+        def recurse(digits: str, index: int, temp: str):
+            for char in characters_map[digits[index]]:
+                temp+=char
+
+
+        return answer
