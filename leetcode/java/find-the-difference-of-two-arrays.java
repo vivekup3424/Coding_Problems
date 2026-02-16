edc8a37b76becbe5f272c40d472a90b4fcbd0b29
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-02-16 06:53:27 +0530

    max-sum

---
+++ leetcode/java/find-the-difference-of-two-arrays.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution{
+    public List<List<Integer>> findDifference(int[] nums1 ,int[] nums2){
+        Set<Integer> set1 = new HashSet<>();
+        for(int num : nums1) set1.add(num);
+
+        Set<Integer> set2 = new HashSet<>();
+        for(int num : nums2) set2.add(num);
+
+        set1.removeAll(set2);
+        set2.removeAll(set1);
+
+        return Arrays.asList(new ArrayList<>(set1), new ArrayList<>(set2));
+    }
+}
