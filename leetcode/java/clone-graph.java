77d69502ab88ddd44e39145181eb6f01848cc5a8
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-02-21 19:02:02 +0530

    clone graph

--- leetcode/java/clone-graph.java
+++ leetcode/java/clone-graph.java
@@ -8,6 +8,26 @@ class Node{
     }
     public Node(int _val){
         val = _val;
+        neighbors = new ArrayList<>();
     }
 }
-class 
+class Solution{
+    public Node cloneGraph(Node node){
+        if(node == null) return null;
+        Map<Node, Node> visited = new HashMap<>();
+        Deque<Node> queue = new ArrayDeque<>();
+        visited.put(node, new Node(node.val));
+        queue.add(node);
+        while(!queue.isEmpty()){
+            Node curr = queue.poll();
+            for(Node neighbor : curr.neighbors){
+                if(!visited.containsKey(neighbor)){
+                    visited.put(neighbor, new Node(neighbor.val));
+                    queue.add(neighbor);
+                }
+                visited.get(curr).neighbors.add(visited.get(neighbor));
+            }
+        }
+        return visited.get(node);
+    }
+}
