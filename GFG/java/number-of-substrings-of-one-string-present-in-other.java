94935ddc6b04762315bef0dd18dc13985baea4f9
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-03-18 18:52:09 +0530

    add some incorrect code

---
+++ GFG/java/number-of-substrings-of-one-string-present-in-other.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Solution{
+    public static void main(String[] args){
+        /**
+         * Suppose we are given a string s1, we need to the find total number of 
+         * substring(including multiple occurrences of the same substring) of s1 
+         * which are present in string s2. `
+         */
+        Scanner scanner = new Scanner(System.in);
+        String pattern, str;
+        System.out.println("Enter the pattern and then the string where we want to have this pattern");
+    }
+}
