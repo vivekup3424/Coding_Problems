a30987cb717f8c261e6ae356c3b3e11df43993db
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-04-02 15:06:00 +0530

    coin change problem

---
+++ GFG/java/CoinChange.java
@@ -0,0 +1,19 @@
+class Solution{
+    public int count(int coins[], int sum){
+        int n = coins.length;
+        int[][] dp = new int[n+1][sum+1];
+        for(int i = 0; i < n; i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= sum; j++){
+                if(coins[i-1] > j){
+                    dp[i][j] = dp[i-1][j];
+                }else {
+                    dp[i][j]= dp[i-1][j] + dp[i-1][j-coins[i-1]];
+                }
+            }
+        }
+    return dp[n][sum];
+    }
+}
