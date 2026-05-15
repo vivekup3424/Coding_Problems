baf4acf9402af22466f9f038b8e6c1a4750bfcce
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-05-15 11:59:18 +0530

    remove concurrency code

--- concurrency_in_java/Counter.java
+++
@@ -1,9 +0,0 @@
-public class Counter {
-    private int count = 0;
-    public synchronized void increment(){
-        count++;
-    }
-    public synchronized int getCount(){
-        return count;
-    }
-}
