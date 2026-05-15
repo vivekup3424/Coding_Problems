7134280eeb97fb05c4da30e03176166eeb1b4254
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-05-15 12:00:04 +0530

    remove a directory

--- concurrency_in_java/RunnableExample.java
+++
@@ -1,8 +0,0 @@
-public class RunnableExample {
-    public static void main(String[] args) {
-        Thread thread1 = new Thread(new MyRunnable());
-        Thread thread2 = new Thread(new MyRunnable());
-        thread1.start();
-        thread2.start();
-    }
-}
