8056243b390c3c16becd12f607584927aab10cd2
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-06-04 14:17:31 +0530

    clean up

--- systemDesign/algomaster/restaurant/my-restaurant/src/main/java/com/restaurant/app/Item.java
+++
@@ -1,8 +0,0 @@
-package com.restaurant.app;
-
-public class Item {
-
-   private String id;
-   private String description;
-   private Float price; 
-}
--- systemDesign/algomaster/restaurant/my-restaurant/src/main/java/com/restaurant/app/Restaurant.java
+++
@@ -1,5 +0,0 @@
-package com.restaurant.app;
-
-public class Restaurant {
-    
-}
