1bf99f9443c4091d3230add797bdd87638ac4115
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-06-04 21:33:58 +0530

    order placed

---
+++ systemDesign/algomaster/Hospital.java
@@ -0,0 +1,30 @@
+import java.util.Date;
+
+public class Hospital {
+    
+}
+
+class Appointment{
+    private Room room;
+    private Date appointmentDate;
+    private Doctor doctor;
+    private 
+
+}
+
+class Doctor{
+
+}
+
+class Patient{
+
+}
+
+class Room{
+    private Integer number;
+    private Integer floor;
+    Room(Integer _number, Integer _floor){
+        this.number = _number;
+        this.floor = _floor;
+    }
+}
