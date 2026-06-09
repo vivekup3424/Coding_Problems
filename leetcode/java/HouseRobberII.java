26e9413c3652126cbca595d09103bcbbe452ec40
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-06-09 18:00:49 +0530

    Fix edge cases and implement HouseRobberII solution

--- leetcode/java/HouseRobberII.java
+++ leetcode/java/HouseRobberII.java
@@ -1,3 +1,11 @@
-public class HouseRobberII {
-    
+class HouseRobberII {
+    public int rob(int[] nums){
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        else if(n==2) return Math.max(nums[1], nums[0]);
+        int first = nums[0], second = nums[1];
+        for (int i = 2; i < n; i++) {
+            int temp = Math.max(nums[i] + first, second);
+        }
+    }
 }
