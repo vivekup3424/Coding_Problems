8daa0199bf0d6d14a2d408bb9d8ee3152d914fd4
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-06-18 00:24:56 +0530

    Add Valid Word Square and Score of a String solutions

---
+++ leetcode/java/ValidWordSquare.java
@@ -0,0 +1,24 @@
+/**
+ * Determine if an array of strings forms a valid word square.
+ *
+ * A word square is a grid where the k-th row equals the k-th column for all valid k.
+ * In other words, words[i][j] == words[j][i] for all i, j.
+ *
+ * Example:
+ *   words = ["abcd", "bnrt", "crmy", "dtye"]
+ *
+ *   a b c d
+ *   b n r t
+ *   c r m y
+ *   d t y e
+ *
+ *   Row 0 == Col 0: "abcd"
+ *   Row 1 == Col 1: "bnrt"
+ *   Row 2 == Col 2: "crmy"
+ *   Row 3 == Col 3: "dtye"  =>  valid
+ */
+public class ValidWordSquare {
+    public isValidWordSquare(String[] words){
+        
+    }
+}
