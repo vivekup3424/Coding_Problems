9cf0212005745dea6f040cd46640718f7fcaad7d
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-06-17 23:38:41 +0530

    Fix logic in appendCharacters method to correctly count characters needed to append

--- leetcode/java/append-characters-to-string-to-make-subsequence.java
+++ leetcode/java/append-characters-to-string-to-make-subsequence.java
@@ -2,8 +2,9 @@ class Solution {
     public int appendCharacters(String s, String t) {
         if(t.length() == 0) return 0;
         int j = 0; //t
-        for(int i = 0; i < s.length(); i++){
-            if(s)
+        for(int i = 0; i < s.length() && j < t.length(); i++){
+            if(s.charAt(i) == t.charAt(j)) j++;
         }
+        return t.length() - j;
     }
 }
