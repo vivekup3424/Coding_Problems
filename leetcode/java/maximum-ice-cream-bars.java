4cbbfdf0e190af6c5b8de1d7e32ee962bcd25987
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-06-21 23:57:35 +0530

    Add MaximumIceCreamBars Java solution

---
+++ leetcode/java/maximum-ice-cream-bars.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count = 0;
+        for(int i = 0; i < costs.length && coins >= costs[i]; i++){
+            
+        }
+        return count;
+    }
+}
