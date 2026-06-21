a41757ee3cb9ff2dddf3246832bd0952a717c561
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-06-21 22:57:29 +0530

    Implement MakingFileNamesUnique solution with visited set logic

--- leetcode/java/making-file-names-unique.java
+++ leetcode/java/making-file-names-unique.java
@@ -1,8 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public String[] getFolderNames(String[] names) {
         int n = names.length;
         String[] ans = new String[n];
-        for (int i = 0; i < ; i++) {
+        Set<String> visited = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            if(visited.contains(visited))
             
         }
         return ans;
