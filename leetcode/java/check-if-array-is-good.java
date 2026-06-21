a2aa73d01f30e5722e4a3cd0ff7355537462f15f
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-06-21 21:57:20 +0530

    Fix CheckIfArrayIsGood logic and add MakingFileNamesUnique stub

--- leetcode/java/check-if-array-is-good.java
+++ leetcode/java/check-if-array-is-good.java
@@ -3,13 +3,15 @@ import java.util.Arrays;
 class Solution{
     public boolean isGood(int[] nums) {
         int n = nums.length;
-        int[] baseArr = new int[n]; //[1...m-1,m,m] => m+1 = n => m = n -1
-        for(int i = 0; i < n-1; i++){
+        int[] baseArr = new int[n]; //[1...m-1,m,m] => m+1 = n => m = n - 1
+        for(int i = 0; i < n-1; i++){ //n-2 = m-1 => baseArr[m-1] = m-1+1
             baseArr[i] = i+1;
         }
-        baseArr[n-1] = n;
-
+        baseArr[n-1] = n-1;
         Arrays.sort(nums);
+        for(int i = 0; i < n; i++){
+            if(baseArr[i] != nums[i]) return false;
+        }
         return true;
     }
 }
---
+++ leetcode/java/making-file-names-unique.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String[] getFolderNames(String[] names) {
+        int n = names.length;
+        String[] ans = new String[n];
+        for (int i = 0; i < ; i++) {
+            
+        }
+        return ans;
+    }
+}
