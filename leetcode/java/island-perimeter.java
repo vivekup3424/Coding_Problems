eece0e595b54a4d48b5644772bce3b6c12c60ba7
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-06-30 15:04:50 +0530

    Simplify island-perimeter solution logic

--- leetcode/java/island-perimeter.java
+++ leetcode/java/island-perimeter.java
@@ -5,7 +5,10 @@ class Solution {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if(grid[i][j] == 1){
-                    
+                    if(i==0 || grid[i-1][j] == 0) perimeter++;
+                    if(j==0 || grid[i][j-1] == 0) perimeter++;
+                    if(i==rows-1 || grid[i+1][j] == 0) perimeter++;
+                    if(i==0 || grid[i-1][j] == 0) perimeter++;
                 }
             }
         }
