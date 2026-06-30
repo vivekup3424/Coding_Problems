c346e6984fea5374138f022f8582639c935aca2c
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-06-30 12:04:33 +0530

    Fix island-perimeter solution logic

--- leetcode/java/island-perimeter.java
+++ leetcode/java/island-perimeter.java
@@ -5,7 +5,7 @@ import java.util.Queue;
 class Solution{
     public int islandPerimeter(int[][] grid){
         int rows = grid.length, cols = grid[0].length;
-        Queue<List<Integer,Integer>> q = new ArrayDeque<>();
+        Queue<List<Integer>> q = new ArrayDeque<>();
         int perimeter = 0;
         int[] dirX = new int[]{0,1,0,-1};
         int[] dirY = new int[]{1,0,-1,0};
@@ -15,6 +15,11 @@ class Solution{
                visited[i][j] = false;
             }
         }
-        q.add()
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j])
+            }
+        }
+
     }
 }
