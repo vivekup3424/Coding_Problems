ca33bdbeeaefab6bcb1843555fc620b9ba7a9f4f
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-06-30 13:04:39 +0530

    Fix island-perimeter solution by removing redundant code

--- leetcode/java/island-perimeter.java
+++ leetcode/java/island-perimeter.java
@@ -5,21 +5,6 @@ import java.util.Queue;
 class Solution{
     public int islandPerimeter(int[][] grid){
         int rows = grid.length, cols = grid[0].length;
-        Queue<List<Integer>> q = new ArrayDeque<>();
-        int perimeter = 0;
-        int[] dirX = new int[]{0,1,0,-1};
-        int[] dirY = new int[]{1,0,-1,0};
-        boolean[][] visited = new boolean[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-               visited[i][j] = false;
-            }
-        }
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(grid[i][j])
-            }
-        }
 
     }
 }
