ea1cc0454ac9e74c5bf96bc1d6c31de3216227ca
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-06-30 14:04:44 +0530

    Refactor island-perimeter solution for clarity

--- leetcode/java/island-perimeter.java
+++ leetcode/java/island-perimeter.java
@@ -1,10 +1,13 @@
-import java.util.ArrayDeque;
-import java.util.List;
-import java.util.Queue;
-
-class Solution{
-    public int islandPerimeter(int[][] grid){
+class Solution {
+    public int islandPerimeter(int[][] grid) {
         int rows = grid.length, cols = grid[0].length;
-
+        int perimeter = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if(grid[i][j] == 1){
+                    
+                }
+            }
+        }
     }
 }
