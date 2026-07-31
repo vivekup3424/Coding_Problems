bac2d241ee9189cec469ec23d249e4e10af40f00
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-07-31 09:39:25 +0530

    Fix duplicate handling and off-by-one bug in 3Sum

--- leetcode/java/3Sum.java
+++ leetcode/java/3Sum.java
@@ -42,9 +42,23 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> answer = new ArrayList<>();
         for(int i = 0; i < n-1; i++){
-            if(nums[i]==nums[i-1])continue;
+            if(i > 0 && nums[i]==nums[i-1])continue;
             int j = i + 1, k = n-1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0){
+                    answer.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while(j < k && nums[j]==nums[j+1])j++;
+                    while(j < k && nums[k]==nums[k-1])k--;
+                    j++;
+                    k--;
+                } else if(sum < 0){
+                    j++;
+                } else {
+                    k--;
+                }
+            }
         }
-
+        return answer;
     }
 }
