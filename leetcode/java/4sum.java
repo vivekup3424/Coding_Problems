6dc1fbd121b3108d1ad63eb2ea120e6efe1c14b5
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-08-01 20:48:07 +0530

    Fix output example and improve bounds check in 4Sum solution

--- leetcode/java/4sum.java
+++ leetcode/java/4sum.java
@@ -12,7 +12,7 @@
  *
  * Example 1:
  *   Input:  nums = [1,0,-1,0,-2,2], target = 0
- *   Output: [[-2,-1,1,2],[-2,0,0,2],[-2,0,1,1],[-1,0,0,1]]
+ *   Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
  *
  * Example 2:
  *   Input:  nums = [2,2,2,2,2], target = 8
@@ -33,11 +33,29 @@ class Solution {
         int n = nums.length;
         for(int i = 0; i < nums.length; i++){
             if(i > 0 && nums[i]==nums[i-1]) continue;
+
             for(int j = i+1; j < nums.length; j++){
-                if(j > 0 && nums[j]==nums[j-1]) continue;
+                if(j > i+1 && nums[j]==nums[j-1]) continue;
+
                 int k = j+1, l = n-1;
-                while
+                while(k < l){
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum == target){
+                        answer.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+                        while(k < l && nums[k]==nums[k-1]) k++;
+                        while(k < l && nums[l]==nums[l+1]) l--;
+                    }
+                    else if(sum > target){
+                        l--;
+                    }
+                    else{
+                        k++;
+                    }
+                }
             }
         }
+        return answer;
     }
 }
