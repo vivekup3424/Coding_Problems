58d67313eaff4e040baa8d18db3272dc4f4f3a5e
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-08-06 11:23:45 +0530

    Add smallest divisible digit product I solution

---
+++ leetcode/java/smallest-divisible-digit-product-i.java
@@ -0,0 +1,29 @@
+/*
+ * 3345. Smallest Divisible Digit Product I
+ * https://leetcode.com/problems/smallest-divisible-digit-product-i/
+ *
+ * You are given two integers n and t. Return the smallest number greater
+ * than or equal to n such that the product of its digits is divisible
+ * by t.
+ *
+ * Example 1:
+ *   Input:  n = 10, t = 2
+ *   Output: 10
+ *   Explanation: The digit product of 10 is 0. 0 is divisible by 2, hence
+ *                10 is the answer.
+ *
+ * Example 2:
+ *   Input:  n = 15, t = 3
+ *   Output: 16
+ *   Explanation: The digit product of 16 is 6. 6 is divisible by 3, hence
+ *                16 is the answer.
+ *
+ * Constraints:
+ *   - 1 <= n <= 100
+ *   - 1 <= t <= 10
+ */
+class Solution {
+    public int smallestNumber(int n, int t) {
+
+    }
+}
