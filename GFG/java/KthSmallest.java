6df063bcc13b820954724d4c6324205e9eb89604
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-08-11 01:39:50 +0530

    Add KthSmallest solution using min-heap approach

--- GFG/java/KthSmallest.java
+++ GFG/java/KthSmallest.java
@@ -1,3 +1,15 @@
+import java.util.*;
+
 class Solution{
-    public int 
+    public int kthSmallest(int[] arr, int k){
+        int n = arr.length;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)->b-a);
+        for(int i = 0; i < n; i++){
+            maxHeap.add(arr[i]);
+            if(maxHeap.size()>k){
+                maxHeap.poll();
+            }
+        }
+        return maxHeap.poll();
+    }
 }
