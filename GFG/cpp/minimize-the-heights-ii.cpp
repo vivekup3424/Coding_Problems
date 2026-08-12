bc3a4eb1da8804c5af7782eafb2daf4eda6becee
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-08-13 01:53:39 +0530

    Fix minimize-the-heights-ii edge case handling

--- GFG/cpp/minimize-the-heights-ii.cpp
+++ GFG/cpp/minimize-the-heights-ii.cpp
@@ -1,5 +1,20 @@
-#include <vect
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
 class Solution{
     public:
-    int getMinDiff()
-}
+    int getMinDiff(vector<int> arr, int k){
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        int ans = arr[n-1] - arr[0];
+        for(int i = 0; i < n-1; i++){
+            if(arr[i+1] - k < 0) continue;
+            int mini = min(arr[0]+k, arr[i+1]-k);
+            int maxi = max(arr[i]+k, arr[n-1]-k);
+            ans = min(ans, maxi - mini);
+        }
+        return ans;
+    }
+};
