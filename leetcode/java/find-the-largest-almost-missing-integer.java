d556a2b6e840022b0f4f60a7dbf969113a0d764b
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-08-18 20:09:17 +0530

    Add find-the-largest-almost-missing-integer solution

---
+++ leetcode/java/find-the-largest-almost-missing-integer.java
@@ -0,0 +1,51 @@
+/*
+ * 3471. Find the Largest Almost Missing Integer
+ * https://leetcode.com/problems/find-the-largest-almost-missing-integer/
+ *
+ * You are given an integer array nums and an integer k.
+ *
+ * An integer x is almost missing from nums if x appears in exactly one
+ * subarray of size k.
+ *
+ * Return the largest almost missing integer from nums. If no such integer
+ * exists, return -1.
+ *
+ * Example 1:
+ *   Input:  nums = [3,9,2,1,7], k = 3
+ *   Output: 7
+ *   Explanation: 1 appears in 2 subarrays of size 3: [9,2,1], [2,1,7].
+ *     2 appears in 3 subarrays of size 3: [3,9,2], [9,2,1], [2,1,7].
+ *     3 appears in 1 subarray of size 3: [3,9,2].
+ *     7 appears in 1 subarray of size 3: [2,1,7].
+ *     9 appears in 2 subarrays of size 3: [3,9,2], [9,2,1].
+ *     We return 7 since it is the largest integer that appears in exactly
+ *     one subarray of size k.
+ *
+ * Example 2:
+ *   Input:  nums = [3,9,7,2,1,7], k = 4
+ *   Output: 3
+ *   Explanation: 1 appears in 2 subarrays of size 4: [9,7,2,1], [7,2,1,7].
+ *     2 appears in 3 subarrays of size 4: [3,9,7,2], [9,7,2,1], [7,2,1,7].
+ *     3 appears in 1 subarray of size 4: [3,9,7,2].
+ *     7 appears in 4 subarrays of size 4.
+ *     9 appears in 3 subarrays of size 4.
+ *     We return 3 since it is the largest integer that appears in exactly
+ *     one subarray of size k.
+ *
+ * Example 3:
+ *   Input:  nums = [0,0], k = 1
+ *   Output: -1
+ *   Explanation: No integer in nums appears in exactly one subarray of size
+ *     k.
+ *
+ * Constraints:
+ *   - 1 <= nums.length <= 50
+ *   - 0 <= nums[i] <= 50
+ *   - 1 <= k <= nums.length
+ */
+
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+
+    }
+}
