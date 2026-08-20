a133ca017bbdbba57aae2c98316a9aef708a86dc
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-08-21 00:18:27 +0530

    Fix longest-k-unique-characters-substring edge case handling

--- GFG/java/longest-k-unique-characters-substring.java
+++ GFG/java/longest-k-unique-characters-substring.java
@@ -1,21 +1,24 @@
 import java.util.HashMap;
 import java.util.Map;
 
-class Solution{
-    public int longestKSubstr(String s, int k){
+class Solution {
+    public int longestKSubstr(String s, int k) {
         int max_length = 0;
         Map<Character, Integer> characterFreqMap = new HashMap<>();
         int j = 0;
-        for(int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             Character c = s.charAt(i);
-            characterFreqMap.put(c,characterFreqMap.getOrDefault(c,0)+1);
-            while(characterFreqMap.size() > k && j < i){
+            characterFreqMap.put(c, characterFreqMap.getOrDefault(c, 0) + 1);
+            while (characterFreqMap.size() > k && j < i) {
                 Character l = s.charAt(j);
-                characterFreqMap.put(l, characterFreqMap.get(l)-1);
-                if(characterFreqMap.get(l)==0) characterFreqMap.remove(l);
+                characterFreqMap.put(l, characterFreqMap.get(l) - 1);
+                if (characterFreqMap.get(l) == 0)
+                    characterFreqMap.remove(l);
                 j++;
             }
-            max_length = Math.max(max_length, i - j + 1);
+            if (characterFreqMap.size() == k) {
+                max_length = Math.max(max_length, i - j + 1);
+            }
         }
         return max_length;
     }
