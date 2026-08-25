8bf0893321d40672fd3059fa9e5e35ede8332cdc
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-08-25 08:03:18 +0530

    Fix can-place-flowers edge case for empty flowerbed

--- leetcode/python/can-place-flowers.py
+++ leetcode/python/can-place-flowers.py
@@ -1,4 +1,10 @@
 from typing import List
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        pass
+        count = 0
+        for i in range(0,len(flowerbed),2):
+            if((i == 0 or flowerbed[i-1] == 0) and flowerbed[i]==0):
+                count+=1
+            if count == n:
+                return True
+        return False
