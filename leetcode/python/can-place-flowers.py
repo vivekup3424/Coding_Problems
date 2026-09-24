a9c35d2cc655a0788c82495bc975d46d42ffa6be
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-09-24 13:11:59 +0530

    WIP: start can-place-flowers Python solution

--- leetcode/python/can-place-flowers.py
+++ leetcode/python/can-place-flowers.py
@@ -1,4 +1,14 @@
 from typing import List
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        pass
+        flower_length = len(flowerbed)
+        if flower_length == 0:
+            return True
+        #start from 0
+        count = 0
+        temp = [flowerbed[i] for i in range(n)]
+        for i in range(0,flower_length):
+            if i == 0 or temp[i-1] == 0:
+                count+=1
+                flowerbed
+
