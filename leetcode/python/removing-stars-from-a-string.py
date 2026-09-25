289c115ed5559f4ae1f05abe345add9be144a0b5
vivekup3424 <vivek.hygfrt3424@gmail.com>
2026-09-25 12:06:53 +0530

    Add Python solutions for decode string and fix star removal

--- leetcode/python/removing-stars-from-a-string.py
+++ leetcode/python/removing-stars-from-a-string.py
@@ -39,4 +39,10 @@ Constraints:
 """
 class Solution:
     def removeStars(self, s: str) -> str:
-        pass
+        stk = []
+        for c in s:
+            if c == '*':
+                stk.pop()
+            else:
+                stk.append(c)
+        return ''.join(stk)
